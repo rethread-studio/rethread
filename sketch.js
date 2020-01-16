7ccfab34248b6cdc357a6575225e276d8e4a1f73
Erik Natanael Gustafsson <erik@eriknatanael.com>
2020-01-16 11:47:52 +0100

    Add video and resize sketch on front page

--- sketch.js
+++ sketch.js
@@ -3,15 +3,39 @@ let nCurves = 200;
 const Y_AXIS = 1;
 const X_AXIS = 2;
 let b1, b2, c1, c2;
+let width = 0;
+let height = 0;
 
+// function windowResized() {
+//     width = window.innerWidth;
+//     height = windowWidth * 0.5;
+//     resizeCanvas(width, height);
+// }
+
+// windowResized function by GoToLoop
+// https://gist.github.com/GoToLoop/54b4c49e9c2541da2d91692bf0c01192
+// removes scrollbars by using the computed width?
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
+    const css = getComputedStyle(document.getElementById('header')),
+          mw = float(css.marginLeft) + float(css.marginRight),
+          mh = float(css.marginTop)  + float(css.marginBottom),
+          ww = float(css.width)  || windowWidth,
+          wh = float(css.height) || windowHeight,
+          w = round(ww - mw), h = round(wh - mh);
+  
+    resizeCanvas(w, h, true);
+  }
 
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
+    let header = document.getElementById('header');
+    width = windowWidth;
+    height = width * 0.5;
+    canvas = createCanvas(width, height);
+    canvas.parent('header');
+    canvas.style('display', 'block');
     canvas.mouseMoved(mouseListener);
     canvas.position(0, 0);
+    windowResized(); // immediately resize to fit
     // canvas.style("z-index", "-1");
 
     background(106, 113, 247);
