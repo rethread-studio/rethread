b07b23cade1f5ec71f3d95b2946fed1640dbfebd
Erik Natanael Gustafsson <erik@eriknatanael.com>
2020-04-17 18:21:57 +0200

    Text on the ideas behind the experiment

--- code/tonejs-experiments/fingerprint-motif-3d/foundational_ideas.md
+++ code/tonejs-experiments/fingerprint-motif-3d/foundational_ideas.md
@@ -1,5 +1,5 @@
 # Foundational ideas
 
-The browser fingerprint contains a reflection of ourselves: our languages, preferred fonts, location, choice of browser. Likewise it reflects the device through the molecules of its graphics card producing slightly different visual noise, the size of the screen, the depth of its pixels.
+The browser fingerprint contains a reflection of the human behind it: their languages, preferred fonts, location, choice of browser. Likewise it reflects the device through the molecules of its graphics card producing slightly different visual noise, the size of the screen, the depth of its pixels.
 
 When a website collects and stores a browser fingerprint, it puts a tiny little trace of the human-computer cyborg on its server. Just the faintest hint of an identity, but oftentimes enough to pinpoint you in a group of hundreds of thousands. Until your human-computer cyborg configuration changes.
--- code/tonejs-experiments/fingerprint-motif-3d/src/globals.js
+++ code/tonejs-experiments/fingerprint-motif-3d/src/globals.js
@@ -44,15 +44,54 @@ var html = {
     <br/><br/><br/><br/><br/>
     <span style="font-size:24px">Click to activate</span>
     <br /><br />
-    Move: WS<br/>
-    Look: MOUSE<br/>
-    Return here: ESC<br/>
-    <br/><br/>
+    Move: W/S <i class="fa fa-keyboard-o" aria-hidden="true"></i>
+    <br/>
+    Look: MOUSE 
+
+    <i class="fa fa-mouse-pointer" aria-hidden="true"></i>
+    <i class="fa fa-arrows-alt" aria-hidden="true"></i>
+    <br/>
+    Return here: ESC <i class="fa fa-keyboard-o" aria-hidden="true"></i><br/>
+    <br/><span style="font-size:36px;"><i class="fa fa-volume-up" aria-hidden="true"></i></span>
+    <br/>
     <span style="font-size:24px; line-height: 2.0em;">Explore browser fingerprints as sonic objects in space. In your presence each device will reveal its sonic identity.</span>`,
+}
+
+var messages = {
+    darkSpaceMessages: [
+        "When a website collects and stores a browser fingerprint, it puts a tiny little trace of the human-computer cyborg on its server. Just the faintest hint of an identity, but oftentimes enough to pinpoint you in a group of hundreds of thousands. Until your human-computer cyborg configuration changes.",
+        "Welcome to the archive",
+    ],
+    insideFingerprintMessages: [
+        "The browser fingerprint contains a reflection of the human behind it: their languages, preferred fonts, location, choice of browser.",
+        "The browser fingerprint reflects its device through the molecules of its graphics card producing slightly different visual noise, the size of the screen, the depth of its pixels.",
+        "The human-computer cyborg is reflected in their data.",
+        "~~~abstract features of HumanConciousness~~~",
+        "Can you hear how similar you are?",
+        "I'm afraid we have no way of putting you in touch with this particular human-computer cyborg.",
+        "Have you ever been attracted to the visual noise of a graphics card?",
+        
+    ],
+    insideSphereMessages: [
+        "The browser fingerprint contains a reflection of the human behind it: their languages, preferred fonts, location, choice of browser. Likewise it reflects the device through the molecules of its graphics card producing slightly different visual noise, the size of the screen, the depth of its pixels.",
+    ]
+}
+
+function getRandomDarkSpaceMessage() {
+    let i = Math.floor(Math.random() * messages.darkSpaceMessages.length);
+    return messages.darkSpaceMessages[i];
+}
+function getRandomInsideFingerprintMessage() {
+    let i = Math.floor(Math.random() * messages.insideFingerprintMessages.length);
+    return messages.insideFingerprintMessages[i];
+}
+function getRandomInsideSphereMessage() {
+    let i = Math.floor(Math.random() * messages.insideSphereMessages.length);
+    return messages.insideSphereMessages[i];
 }
 
 var state = {
     mobile: false,
 }
 
-export { sound, data, html, state }
+export { sound, data, html, state, getRandomDarkSpaceMessage, getRandomInsideFingerprintMessage, getRandomInsideSphereMessage }
