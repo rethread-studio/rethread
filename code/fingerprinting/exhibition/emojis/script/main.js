085ea459e4a54bb3223bf59c58e8aba0413e40a5
tdurieux <durieuxthomas@hotmail.com>
2020-04-19 17:07:23 +0200

    improve emojis exhibition

--- code/fingerprinting/exhibition/emojis/script/main.js
+++ code/fingerprinting/exhibition/emojis/script/main.js
@@ -8,4 +8,15 @@ ctx.fillStyle = "rgb(0, 0, 0)";
 ctx.strokeStyle = ctx.fillStyle;
 ctx.textAlign = "center";
 ctx.fillText("😃 🦠 🎉 🚀 🖼️", canvas.width / 2, canvas.height / 2);
-console.log(canvas.toDataURL())
+
+setInterval(() => {
+    getRandomFingerPrint(fp => {
+        document.getElementById('other').src = (fp.original.emojis)
+    })
+}, 500)
+
+$("#seeArt").click(() => {
+    $("#content").toggleClass('art');
+    $("#info").fadeOut();
+  })
+  
