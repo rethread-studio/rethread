e09b89ac54c0b9cf886497d7b0469b66276cfd72
Erik Natanael Gustafsson <erik@eriknatanael.com>
2020-08-21 19:01:34 +0200

    Particles drawn per service at a certain location

--- code/BCM/server/public/js/visualization.js
+++ code/BCM/server/public/js/visualization.js
@@ -3,6 +3,12 @@ import Stats from "https://unpkg.com/three@0.119.1/examples/jsm/libs/stats.modul
 import { GUI } from "https://unpkg.com/three@0.119.1/examples/jsm/libs/dat.gui.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.119.1/examples/jsm/controls/OrbitControls.js";
 
+let allPackets = [];
+const statsPerService = new Map();
+const positionPerService = new Map();
+
+
+
 let protocol = "ws";
 if (document.location.protocol == "https:") {
   protocol += "s";
@@ -17,8 +23,44 @@ ws.onmessage = (message) => {
   const json = JSON.parse(message.data);
   if (json.event == "networkActivity") {
     const packet = json.data;
+    // console.log(packet);
+    allPackets.push({
+      location: packet.location,
+    })
+
+    for(const service of packet.services) {
+      if (!positionPerService.has(service)) {
+        positionPerService.set(service, random3DPosition(500));
+        console.log("new serivce, num: " + positionPerService.size);
+      }
+      addParticle(positionPerService.get(service));
+    }
   }
 };
+
+function random3DPosition(magnitude) {
+  return new THREE.Vector3(
+    (-1 + Math.random() * 2) * magnitude,
+    (-1 + Math.random() * 2) * magnitude,
+    (-1 + Math.random() * 2) * magnitude,
+  )
+}
+
+function addParticle(vec3) {
+  let i = particleIndex;
+  particlePositions[i * 3] = vec3.x;
+  particlePositions[i * 3 + 1] = vec3.y;
+  particlePositions[i * 3 + 2] = vec3.z;
+  particleIndex++;
+  if(particleIndex >= maxParticleCount) {
+    particleIndex = 0;
+  }
+  particleCount++;
+  if(particleCount > maxParticleCount) {
+    particleCount = maxParticleCount;
+  }
+  particles.setDrawRange(0, particleCount);
+}
 
 var group;
 var container, stats;
@@ -30,8 +72,9 @@ var pointCloud;
 var particlePositions;
 var linesMesh;
 
-var maxParticleCount = 1000;
-var particleCount = 500;
+var maxParticleCount = 50000;
+var particleCount = 0; // The number of active particles
+var particleIndex = 0; // The index of the next particle (can loop around to recycle old particles)
 var r = 800;
 var rHalf = r / 2;
 
@@ -41,7 +84,7 @@ var effectController = {
   minDistance: 150,
   limitConnections: false,
   maxConnections: 20,
-  particleCount: 500,
+  particleCount: 0,
 };
 
 init();
@@ -89,23 +132,24 @@ function init() {
   group = new THREE.Group();
   scene.add(group);
 
-  var helper = new THREE.BoxHelper(
-    new THREE.Mesh(new THREE.BoxBufferGeometry(r, r, r))
-  );
-  helper.material.color.setHex(0x101010);
-  helper.material.blending = THREE.AdditiveBlending;
-  helper.material.transparent = true;
-  group.add(helper);
+  // var helper = new THREE.BoxHelper(
+  //   new THREE.Mesh(new THREE.BoxBufferGeometry(r, r, r))
+  // );
+  // helper.material.color.setHex(0x101010);
+  // helper.material.blending = THREE.AdditiveBlending;
+  // helper.material.transparent = true;
+  // group.add(helper);
 
-  var segments = maxParticleCount * maxParticleCount;
+  // var segments = maxParticleCount * maxParticleCount;
 
-  positions = new Float32Array(segments * 3);
-  colors = new Float32Array(segments * 3);
+  // positions = new Float32Array(segments * 3);
+  // colors = new Float32Array(segments * 3);
 
   var pMaterial = new THREE.PointsMaterial({
     color: 0xffffff,
-    size: 3,
+    size: 1,
     blending: THREE.AdditiveBlending,
+    opacity: 0.2,
     transparent: true,
     sizeAttenuation: false,
   });
@@ -114,20 +158,24 @@ function init() {
   particlePositions = new Float32Array(maxParticleCount * 3);
 
   for (var i = 0; i < maxParticleCount; i++) {
-    var x = Math.random() * r - r / 2;
-    var y = Math.random() * r - r / 2;
-    var z = Math.random() * r - r / 2;
+    // var x = Math.random() * r - r / 2;
+    // var y = Math.random() * r - r / 2;
+    // var z = Math.random() * r - r / 2;
 
-    particlePositions[i * 3] = x;
-    particlePositions[i * 3 + 1] = y;
-    particlePositions[i * 3 + 2] = z;
+    // particlePositions[i * 3] = x;
+    // particlePositions[i * 3 + 1] = y;
+    // particlePositions[i * 3 + 2] = z;
+
+    particlePositions[i * 3] = 0;
+    particlePositions[i * 3 + 1] = 0;
+    particlePositions[i * 3 + 2] = 0;
 
     // add it to the geometry
     particlesData.push({
       velocity: new THREE.Vector3(
-        -1 + Math.random() * 2,
-        -1 + Math.random() * 2,
-        -1 + Math.random() * 2
+        -.1 + Math.random() * .2,
+        -.1 + Math.random() * .2,
+        -.1 + Math.random() * .2
       ),
       numConnections: 0,
     });
@@ -145,29 +193,6 @@ function init() {
   pointCloud = new THREE.Points(particles, pMaterial);
   group.add(pointCloud);
 
-  var geometry = new THREE.BufferGeometry();
-
-  geometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(positions, 3).setUsage(THREE.DynamicDrawUsage)
-  );
-  geometry.setAttribute(
-    "color",
-    new THREE.BufferAttribute(colors, 3).setUsage(THREE.DynamicDrawUsage)
-  );
-
-  geometry.computeBoundingSphere();
-
-  geometry.setDrawRange(0, 0);
-
-  var material = new THREE.LineBasicMaterial({
-    vertexColors: true,
-    blending: THREE.AdditiveBlending,
-    transparent: true,
-  });
-
-  linesMesh = new THREE.LineSegments(geometry, material);
-  group.add(linesMesh);
 
   //
   renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -206,71 +231,67 @@ function animate() {
     particlePositions[i * 3 + 1] += particleData.velocity.y;
     particlePositions[i * 3 + 2] += particleData.velocity.z;
 
-    if (
-      particlePositions[i * 3 + 1] < -rHalf ||
-      particlePositions[i * 3 + 1] > rHalf
-    )
-      particleData.velocity.y = -particleData.velocity.y;
+    // if (
+    //   particlePositions[i * 3 + 1] < -rHalf ||
+    //   particlePositions[i * 3 + 1] > rHalf
+    // )
+    //   particleData.velocity.y = -particleData.velocity.y;
 
-    if (particlePositions[i * 3] < -rHalf || particlePositions[i * 3] > rHalf)
-      particleData.velocity.x = -particleData.velocity.x;
+    // if (particlePositions[i * 3] < -rHalf || particlePositions[i * 3] > rHalf)
+    //   particleData.velocity.x = -particleData.velocity.x;
 
-    if (
-      particlePositions[i * 3 + 2] < -rHalf ||
-      particlePositions[i * 3 + 2] > rHalf
-    )
-      particleData.velocity.z = -particleData.velocity.z;
+    // if (
+    //   particlePositions[i * 3 + 2] < -rHalf ||
+    //   particlePositions[i * 3 + 2] > rHalf
+    // )
+    //   particleData.velocity.z = -particleData.velocity.z;
 
-    if (
-      effectController.limitConnections &&
-      particleData.numConnections >= effectController.maxConnections
-    )
-      continue;
+    // if (
+    //   effectController.limitConnections &&
+    //   particleData.numConnections >= effectController.maxConnections
+    // )
+    //   continue;
 
-    // Check collision
-    for (var j = i + 1; j < particleCount; j++) {
-      var particleDataB = particlesData[j];
-      if (
-        effectController.limitConnections &&
-        particleDataB.numConnections >= effectController.maxConnections
-      )
-        continue;
+    // // Check collision
+    // for (var j = i + 1; j < particleCount; j++) {
+    //   var particleDataB = particlesData[j];
+    //   if (
+    //     effectController.limitConnections &&
+    //     particleDataB.numConnections >= effectController.maxConnections
+    //   )
+    //     continue;
 
-      var dx = particlePositions[i * 3] - particlePositions[j * 3];
-      var dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
-      var dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
-      var dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+    //   var dx = particlePositions[i * 3] - particlePositions[j * 3];
+    //   var dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
+    //   var dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
+    //   var dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-      if (dist < effectController.minDistance) {
-        particleData.numConnections++;
-        particleDataB.numConnections++;
+    //   if (dist < effectController.minDistance) {
+    //     particleData.numConnections++;
+    //     particleDataB.numConnections++;
 
-        var alpha = 1.0 - dist / effectController.minDistance;
+    //     var alpha = 1.0 - dist / effectController.minDistance;
 
-        positions[vertexpos++] = particlePositions[i * 3];
-        positions[vertexpos++] = particlePositions[i * 3 + 1];
-        positions[vertexpos++] = particlePositions[i * 3 + 2];
+    //     positions[vertexpos++] = particlePositions[i * 3];
+    //     positions[vertexpos++] = particlePositions[i * 3 + 1];
+    //     positions[vertexpos++] = particlePositions[i * 3 + 2];
 
-        positions[vertexpos++] = particlePositions[j * 3];
-        positions[vertexpos++] = particlePositions[j * 3 + 1];
-        positions[vertexpos++] = particlePositions[j * 3 + 2];
+    //     positions[vertexpos++] = particlePositions[j * 3];
+    //     positions[vertexpos++] = particlePositions[j * 3 + 1];
+    //     positions[vertexpos++] = particlePositions[j * 3 + 2];
 
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
+    //     colors[colorpos++] = alpha;
+    //     colors[colorpos++] = alpha;
+    //     colors[colorpos++] = alpha;
 
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
+    //     colors[colorpos++] = alpha;
+    //     colors[colorpos++] = alpha;
+    //     colors[colorpos++] = alpha;
 
-        numConnected++;
-      }
-    }
+    //     numConnected++;
+    //   }
+    // }
   }
-
-  linesMesh.geometry.setDrawRange(0, numConnected * 2);
-  linesMesh.geometry.attributes.position.needsUpdate = true;
-  linesMesh.geometry.attributes.color.needsUpdate = true;
 
   pointCloud.geometry.attributes.position.needsUpdate = true;
 
