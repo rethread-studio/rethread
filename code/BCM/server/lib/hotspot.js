5a1c46437537cc6c71515812f2ad44db1e3a6b63
tdurieux <durieuxthomas@hotmail.com>
2020-08-26 11:18:19 +0200

    ban wifi client

--- code/BCM/server/lib/hotspot.js
+++ code/BCM/server/lib/hotspot.js
@@ -1,8 +1,18 @@
 const arp = require("./arp");
 const ping = require("ping");
 const sh = require("shelljs");
+const listwificlients = require("listwificlients").default;
 
-module.exports.connectedUsers = async (interface) => {
+module.exports.isHotspot = () => {
+  try {
+    const output = sh.which("hostapd");
+    return output != null;
+  } catch (error) {
+    return false;
+  }
+};
+
+const arpGetUsers = async (interface) => {
   const devices = (
     await arp({
       interface,
@@ -16,9 +26,7 @@ module.exports.connectedUsers = async (interface) => {
   });
   const promises = [];
   for (let device of devices) {
-    promises.push(
-      ping.promise.probe(device.ip, { min_reply: 2, timeout: 1 })
-    );
+    promises.push(ping.promise.probe(device.ip, { min_reply: 2, timeout: 1 }));
   }
   const results = await Promise.all(promises);
   for (let index = 0; index < devices.length; index++) {
@@ -28,6 +36,28 @@ module.exports.connectedUsers = async (interface) => {
   return devices.filter((f) => f.alive);
 };
 
+module.exports.connectedUsers = async (interface) => {
+  if (module.exports.isHotspot()) {
+    const clients = listwificlients(interface);
+    const devices = await arp({
+      interface,
+    });
+    const output = [];
+    for (let client of clients) {
+      for (let device of devices) {
+        if (client.mac == device.mac) {
+          device.signal = client.signal;
+          device.signalMin = client.signalMin;
+          device.signalMax = client.signalMax;
+        }
+      }
+    }
+    return output;
+  } else {
+    return await arpGetUsers(interface);
+  }
+};
+
 module.exports.isConnected = async (interface) => {
   return (await module.exports.connectedUsers(interface)).length > 0;
 };
@@ -35,4 +65,11 @@ module.exports.isConnected = async (interface) => {
 module.exports.disconnect = async (client_mac) => {
   sh.exec(`sudo hostapd_cli deauthenticate ${client_mac}`, { silent: true });
   sh.exec(`sudo hostapd_cli disassociate ${client_mac}`, { silent: true });
+  sh.exec(`echo ${client_mac} > /home/pi/hostapd.deny`, { silent: true });
+  sh.exec(`sudo /etc/init.d/hostapd reload`, { silent: true });
+};
+
+module.exports.unban = async () => {
+  sh.exec(`echo '' > /home/pi/hostapd.deny`, { silent: true });
+  sh.exec(`sudo /etc/init.d/hostapd reload`, { silent: true });
 };
