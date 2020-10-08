76331d8878f77be3419c11c0c97a79171c178c30
tdurieux <durieuxthomas@hotmail.com>
2020-10-08 15:25:35 +0200

    send idle and home events to osc

--- code/BCM/browser/server/index.js
+++ code/BCM/browser/server/index.js
@@ -43,10 +43,12 @@ wss.on("connection", function (ws, request) {
       }
     }
     if (message.event == "idle") {
+      osc.send(message.action, message.event);
       console.log(message.action);
     }
     if (message.event == "home") {
       console.log(message);
+      osc.send(message.action, message.event);
     }
     if (message.request) {
       if (message.request.responseHeaders) {
