1782eecdc861f75128da83b272c8d45c893cecae
imjhonny <ramjonathan@gmail.com>
2020-10-29 14:03:36 +0100

    fix size

--- code/BCM/browser/public/js/visualization.js
+++ code/BCM/browser/public/js/visualization.js
@@ -793,7 +793,7 @@ function init() {
   if (options.installation) {
     renderer.setSize(window.innerWidth / 2, 1220, false);
   } else {
-    renderer.setSize(window.innerWidth / 2, window.innerHeight, false);
+    renderer.setSize(window.innerWidth / 2, 1220, false);
   }
 
   renderer.outputEncoding = THREE.sRGBEncoding;
