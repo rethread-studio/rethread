4a9ba5559fb84d36efa0ab4233f8125b6882fdcd
Erik Natanael Gustafsson <erik@eriknatanael.com>
2020-11-24 14:48:50 +0100

    Ports last

--- code/nobel/rfc_675_08-architecture/js/score.js
+++ code/nobel/rfc_675_08-architecture/js/score.js
@@ -14,30 +14,103 @@
 // Fade out is not a section, it is instead triggered
 var score = [
 
-  // {
-  //   name: "intro",
-  //   sections: [
-  //     { name: "in", duration: 3 },
-  //     { name: "fade out", duration: 7 },
-  //   ],
-  // },
-  // {
-  //   name: "world",
-  //   sections: [
-  //     { name: "SWEDEN", duration: 12 },
-  //     { name: "EU", duration: 12 },
-  //     { name: "AME", duration: 12 },
-  //     { name: "AS", duration: 12 },
-  //     { name: "fade out", duration: 7 },
-  //   ],
-  // },
-  // {
-  //   name: "intro",
-  //   sections: [
-  //     { name: "transition", duration: 1 },
-  //     { name: "fade out", duration: 7 },
-  //   ],
-  // },
+  {
+    name: "intro",
+    sections: [
+      { name: "in", duration: 3 },
+      { name: "fade out", duration: 4 },
+    ],
+  },
+  {
+    name: "world",
+    sections: [
+      { name: "SWEDEN", duration: 12 },
+      { name: "EU", duration: 12 },
+      { name: "AME", duration: 12 },
+      { name: "AS", duration: 12 },
+      { name: "fade out", duration: 4 },
+    ],
+  },
+  {
+    name: "intro",
+    sections: [
+      { name: "transition", duration: 1 },
+      { name: "fade out", duration: 4 },
+    ],
+  },
+  {
+    name: "numbers",
+    sections: [
+      { name: "all", duration: 10, region: "Sweden", speed: 0.5 },
+      { name: "all", duration: 13, region: "Europe", speed: 0.8 },
+      { name: "all", duration: 18, region: "The World", speed: 1.1 },
+      {
+        name: "in",
+        duration: 14,
+        region: "The World",
+        textLimit: 8,
+        speed: 1.0,
+      },
+      {
+        name: "out",
+        duration: 18,
+        region: "The World",
+        textLimit: 4,
+        speed: 1.5,
+      },
+      {
+        name: "size",
+        duration: 20,
+        startTextLimit: 4,
+        endTextLimit: 2.5,
+        startSpeed: 0.4,
+        endSpeed: 1.5,
+      },
+      {
+        name: "multinumbers",
+        duration: 15,
+        region: "none",
+        textLimit: 2,
+        startSpeed: 1.5,
+        endSpeed: 1.2,
+      },
+      {
+        name: "pre fade out",
+        startTextLimit: 2,
+        endTextLimit: 9,
+        duration: 3,
+        startSpeed: 1.5,
+        endSpeed: 0.4,
+      },
+      { name: "fade out", duration: 20 },
+    ],
+  },
+  {
+    name: "intro",
+    sections: [
+      { name: "in", duration: 3 },
+      { name: "fade out", duration: 4 },
+    ],
+  },
+  {
+    name: "drops",
+    sections: [
+      { name: "in", duration: 12, region: "Sweden" },
+      { name: "out", duration: 12, region: "Sweden" },
+      { name: "in", duration: 16, region: "Europe" },
+      { name: "out", duration: 16, region: "Europe" },
+      { name: "in", duration: 20, region: "none" },
+      { name: "out", duration: 20, region: "none" },
+      { name: "fade out", duration: 4 },
+    ],
+  },
+  {
+    name: "intro",
+    sections: [
+      { name: "transition", duration: 3 },
+      { name: "fade out", duration: 4 },
+    ],
+  },
   {
     name: "ports",
     sections: [
@@ -65,122 +138,7 @@ var score = [
       { name: "network", duration: 0.5, pullBackCoeff: 1000.0 },
       { name: "packets", duration: 0.5, pullBackCoeff: 0.0 },
       { name: "network", duration: 0.5, pullBackCoeff: 1000.0 },
-      { name: "fade out", duration: 5 },
-    ],
-  },
-  {
-    name: "intro",
-    sections: [
-      { name: "transition", duration: 1 },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "numbers",
-    sections: [
-      { name: "all", duration: 10, region: "Sweden", speed: 0.5 },
-      { name: "all", duration: 13, region: "Europe", speed: 0.8 },
-      { name: "all", duration: 18, region: "The World", speed: 1.1 },
-      {
-        name: "in",
-        duration: 14,
-        region: "The World",
-        textLimit: 8,
-        speed: 1.0,
-      },
-      {
-        name: "out",
-        duration: 18,
-        region: "The World",
-        textLimit: 4,
-        speed: 1.5,
-      },
-      {
-        name: "size",
-        duration: 20,
-        startTextLimit: 4,
-        endTextLimit: 2,
-        startSpeed: 0.4,
-        endSpeed: 1.5,
-      },
-      {
-        name: "multinumbers",
-        duration: 15,
-        region: "none",
-        textLimit: 0,
-        startSpeed: 1.5,
-        endSpeed: 1.2,
-      },
-      {
-        name: "pre fade out",
-        textLimit: 0,
-        duration: 3,
-        startSpeed: 1.5,
-        endSpeed: 0.4,
-      },
-      { name: "fade out", duration: 20 },
-    ],
-  },
-  {
-    name: "intro",
-    sections: [
-      { name: "in", duration: 1, region: "Sweden" },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "drops",
-    sections: [
-      { name: "in", duration: 12, region: "Sweden" },
-      { name: "out", duration: 12, region: "Sweden" },
-      { name: "in", duration: 16, region: "Europe" },
-      { name: "out", duration: 16, region: "Europe" },
-      { name: "in", duration: 20, region: "none" },
-      { name: "out", duration: 20, region: "none" },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "intro",
-    sections: [
-      { name: "transition", duration: 1 },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "drops",
-    sections: [
-      { name: "in", duration: 10, region: "Sweden" },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "intro",
-    sections: [
-      { name: "transition", duration: 1 },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "numbers",
-    sections: [
-      { name: "all", duration: 10, region: "Sweden" },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "intro",
-    sections: [
-      { name: "transition", duration: 1 },
-      { name: "fade out", duration: 7 },
-    ],
-  },
-  {
-    name: "ports",
-    sections: [
-      { name: "network", duration: 5, pullBackCoeff: 10.0 },
-      { name: "packets", duration: 5, pullBackCoeff: 10.0 },
-      { name: "fade out", duration: 5 },
+      { name: "fade out", duration: 3 },
     ],
   },
   {
