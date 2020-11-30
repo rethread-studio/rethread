b5aea3d5edecd9dd2e0e0a5f743eb3b71d6d4625
Erik Natanael Gustafsson <erik@eriknatanael.com>
2020-11-30 18:38:16 +0100

    Glitchy sonification and the start of a spatial communications movement

--- code/nobel/sonification/ws_osc_relay/index.js
+++ code/nobel/sonification/ws_osc_relay/index.js
@@ -9,8 +9,6 @@ const options = cli.parse({
 
 let client;
 
-var doLog = true;
-
 var allCountries = new Set();
 var allContinents = new Set();
 
@@ -18,12 +16,12 @@ function connectToServer() {
     client = new WebSocket('ws://nobel.durieux.me:1189');
     client.on('open', () => {
         console.log("Connected to server via websocket");
-        heartbeat(client);
+        // heartbeat(client);
     });
 
     client.on('ping', heartbeat);
     client.on('close', function clear() {
-        clearTimeout(this.pingTimeout);
+        // clearTimeout(this.pingTimeout);
         connectToServer();
     });
 
@@ -42,23 +40,30 @@ function connectToServer() {
           country = internalData.local_location.country;
           continent = internalData.local_location.continent;
         }
+        if(country == "Curaçao") {
+          // This is needed to stop a weird segfault in SuperCollider
+          // more info: https://github.com/supercollider/supercollider/issues/5267
+          country = "Curacao";
+        }
         let args = [];
         args.push({ type: "i", value: internalData.len })
         args.push({ type: "i", value: internalData.out })
-        args.push({ type: "s", value: country })
+        args.push({ type: "i", value: internalData.local_port })
+        args.push({ type: "i", value: internalData.remove_port })
         args.push({ type: "s", value: continent })
+        args.push({ type: "s", value: country })
 
-        if(!allCountries.has(country)) {
-          console.log(country);
-          allCountries.add(country);
-        }
-        if(!allContinents.has(continent)) {
-          console.log(continent);
-          allContinents.add(continent);
-        }
-        if(doLog) {
-          // console.log(args);
-        }
+        // if(!allCountries.has(country)) {
+        //   if(country == "Curaçao")
+        //     console.log(country);
+        //   if(country == "Curaçao ")
+        //     console.log("with space: " + country);
+        //   allCountries.add(country);
+        // }
+        // if(!allContinents.has(continent)) {
+        //   console.log(continent);
+        //   allContinents.add(continent);
+        // }
         udpPort.send(
             {
               address: "/data",
@@ -78,7 +83,7 @@ function heartbeat(ws) {
   // Delay should be equal to the interval at which your server
   // sends out pings plus a conservative assumption of the latency.
   ws.pingTimeout = setTimeout(() => {
-    ws.close();
+    ws.terminate();
   }, 30000 + 1000);
 }
 
