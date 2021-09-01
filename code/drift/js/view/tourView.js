6dbeb5dca37d9d60434a9bfdeeb73e6538098de5
imjhonny <ramjonathan@gmail.com>
2021-09-01 15:06:11 +0200

    add Drift trailer

--- code/drift/js/view/tourView.js
+++ code/drift/js/view/tourView.js
@@ -9,7 +9,8 @@ export default class TourView {
         var content = `
         
         <div class="relative w-screen h-screen">
-            <iframe src="https://player.vimeo.com/video/483464921?autoplay=1&color=c9ff23&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+        </iframe>
+            <iframe src="https://player.vimeo.com/video/595898093?autoplay=1&color=c9ff23&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
             </iframe>    
         </div>
           
