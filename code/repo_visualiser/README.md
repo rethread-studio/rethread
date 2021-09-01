1cec8ac8494ea54f8df6ba2394714f6bccd9a177
Erik Natanael Gustafsson <erik@eriknatanael.com>
2021-09-01 18:05:49 +0200

    Change to a working commit jsonizer and produce first vis

--- code/repo_visualiser/README.md
+++ code/repo_visualiser/README.md
@@ -7,4 +7,9 @@ That means that you need to be using a nightly Rust compiler and have lld instal
 
 ## Generation of commit JSON
 
-The generation of the commit history json file was done using [Gitlogg](https://github.com/dreamyguy/gitlogg)
+Several approaches to rendering the commits and the stats for each commit have been tried. Most of them break at some point or another.
+
+[Gitlogg](https://github.com/dreamyguy/gitlogg)
+
+In the end, the one liners in the following repo worked, with some manual intervention to clean up the data:
+https://github.com/context-driven-testing-toolkit/git-log2json
--- code/repo_visualiser/src/commits.rs
+++ code/repo_visualiser/src/commits.rs
@@ -2,10 +2,86 @@ use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
 use serde_with::serde_as;
 use serde_with::DisplayFromStr;
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Deserialize)]
+struct CommitRaw {
+    commit: String,
+    author: String,
+    date: String,
+    timestamp: u64,
+    message: String,
+    repo: String,
+}
+
+#[derive(Deserialize)]
+struct StatRaw {
+    commit: String,
+    stats: Vec<FileChange>,
+}
 
 #[serde_as]
 #[derive(Deserialize)]
+pub struct FileChange {
+    #[serde_as(as = "DisplayFromStr")]
+    insertions: i32,
+    #[serde_as(as = "DisplayFromStr")]
+    deletions: i32,
+    path: String,
+}
+
+/// Unified commit object containing all the data we're interested in
+
 pub struct Commit {
+    pub commit: String,
+    pub author: String,
+    pub timestamp: u64,
+    pub message: String,
+    pub files_changed: Vec<FileChange>,
+    pub insertions: i32,
+    pub deletions: i32,
+    pub impact: i32,
+}
+
+pub fn read_commit_data(commit_path: PathBuf, stat_path: PathBuf) -> Vec<Commit> {
+    let commit_data = fs::read_to_string(commit_path).expect("couldn't read file");
+    let commits: Vec<CommitRaw> = serde_json::from_str(&commit_data).expect("Failed to parse json");
+
+    let stat_data = fs::read_to_string(stat_path).expect("couldn't read file");
+    let mut stats: HashMap<String, Vec<FileChange>> =
+        serde_json::from_str(&stat_data).expect("Failed to parse json");
+
+    let mut returned_commits = vec![];
+    for c in commits {
+        let mut commit = Commit {
+            commit: c.commit,
+            author: c.author,
+            timestamp: c.timestamp,
+            message: c.message,
+            files_changed: Vec::new(),
+            insertions: 0,
+            deletions: 0,
+            impact: 0,
+        };
+        // remove and return the list of file changes if one exists for the commit
+        if let Some(commit_stats) = stats.remove(&commit.commit) {
+            for fc in &commit_stats {
+                commit.insertions += fc.insertions;
+                commit.deletions += fc.deletions;
+            }
+            commit.files_changed = commit_stats;
+            commit.impact = commit.insertions - commit.deletions;
+        }
+        returned_commits.push(commit);
+    }
+    returned_commits
+}
+
+#[serde_as]
+#[derive(Deserialize)]
+pub struct CommitGitlogg {
     pub repository: String,
     pub commit_nr: i32,
     pub commit_hash: String,
