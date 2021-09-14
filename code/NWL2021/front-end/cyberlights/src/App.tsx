084ccee27c8147c22db419d984a78a53c47e9643
imjhonny <ramjonathan@gmail.com>
2021-09-14 10:41:23 +0200

    add basic setup and loading

---
+++ code/NWL2021/front-end/cyberlights/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Loading } from './components/loading';
+
+function App() {
+  return (
+    <div className="container h-screen bg-gray-900">
+      <Loading />
+    </div>
+  );
+}
+
+export default App;
