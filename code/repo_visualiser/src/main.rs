14f9a0f264341d6977fda611da85138fefe45772
Erik Natanael Gustafsson <erik@eriknatanael.com>
2021-09-14 13:16:43 +0200

    Draw repository files as circles

--- code/repo_visualiser/src/main.rs
+++ code/repo_visualiser/src/main.rs
@@ -1,11 +1,54 @@
 use nannou::prelude::*;
 use std::fs;
 mod commits;
+use chrono::{DateTime, Utc};
 use commits::read_commit_data;
 use commits::Commit;
+use indexmap::IndexMap;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
-use chrono::{DateTime, Utc};
+// Visualisation planning:
+// 1. Tracing files over time, watching the repo grow.
+// Track every file, when it first starts existing to when it stops, and
+// every changes in between. Save in a HashMap.
+// 2. Draw several visualisations on top of each other.
+// 3. Draw lines between commits by the same author, in the order they come. These lists, with indices for commits, can be generated during setup.
+// 4. Draw lines between commits and the files they change.
+// 5. There could be a time element where things fade. The current state of a file is fully opaque, its history is faded a little bit, like a shadow.
+// 6. Don't plot over time, just draw each file as a blob.
+// 7. Draw commits or files as sine waves that lose amplitude over time.
+
+/// Blob is our files with their position and other attributes necessary to draw them
+struct Blob {
+    pos: Point2,
+    color: Hsla,
+    radius: f32,
+}
+
+impl Blob {
+    pub fn overlaps(&self, pos: Point2, r: f32) -> bool {
+        let rel_pos = self.pos - pos;
+        self.pos.distance_squared(pos) < (self.radius + r).powi(2)
+    }
+}
+
+#[derive(Clone)]
+struct FileChange {
+    author: usize,
+    size: i32,
+    timestamp: u64,
+}
+#[derive(Clone)]
+struct FileHistory {
+    start_commit_index: usize,
+    end_commit_index: usize,
+    start_timestamp: u64,
+    num_changes: u32,
+    path: String,
+    /// How large the file is at each commit
+    changes: Vec<FileChange>,
+}
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -13,6 +56,12 @@ fn main() {
 
 struct Model {
     commits: Vec<Commit>,
+    /// Changes per file
+    file_history: IndexMap<String, FileHistory>,
+    /// Commits by a certain author, in order
+    author_history: HashMap<String, Vec<usize>>,
+    first_timestamp: u64,
+    last_timestamp: u64,
     // The texture that we will draw to.
     texture: wgpu::Texture,
     // Create a `Draw` instance for drawing to our texture.
@@ -24,6 +73,9 @@ struct Model {
     // The type used to resize our texture to the window texture.
     texture_reshaper: wgpu::TextureReshaper,
     screenshot: bool,
+    zoom: f32,
+    focus_point: Point2,
+    blobs: Vec<Blob>,
 }
 
 fn model(app: &App) -> Model {
@@ -97,14 +149,129 @@ fn model(app: &App) -> Model {
     let mut stat_path = filepath.clone();
     stat_path.push("rethread_stats.json");
     let commits = read_commit_data(commit_path, stat_path);
+
+    let mut first_timestamp = u64::MAX;
+    let mut last_timestamp = 0;
+
+    let mut author_history = HashMap::new();
+    let mut file_history = IndexMap::new();
+    let mut author_indices = IndexMap::new();
+    for (i, c) in commits.iter().rev().enumerate() {
+        if c.timestamp > last_timestamp {
+            last_timestamp = c.timestamp;
+        }
+        if c.timestamp < first_timestamp {
+            first_timestamp = c.timestamp;
+        }
+        // Add to author history
+        let author_entry = author_history.entry(c.author.clone()).or_insert(Vec::new());
+        author_entry.push(i);
+        for file in &c.files_changed {
+            // Get entry and add new if it doesn't exist
+            let file_entry = file_history
+                .entry(file.path.clone())
+                .or_insert(FileHistory {
+                    start_commit_index: i,
+                    end_commit_index: i,
+                    start_timestamp: c.timestamp,
+                    num_changes: 0,
+                    path: file.path.clone(),
+                    changes: Vec::new(),
+                });
+            // Set the current commit as last commit
+            file_entry.end_commit_index = i;
+            // Add a size entry to the file history
+            let size_change = file.insertions - file.deletions;
+            let num_authors = author_indices.len();
+            let author_index = author_indices.entry(&c.author).or_insert(num_authors);
+            let new_size = if let Some(change) = file_entry.changes.last() {
+                change.size + size_change
+            } else {
+                size_change
+            };
+            let file_change = FileChange {
+                author: *author_index,
+                size: new_size,
+                timestamp: c.timestamp,
+            };
+            file_entry.changes.push(file_change);
+        }
+    }
+
+    let mut max_size = 0;
+    for fh in file_history.values() {
+        let s = fh.changes.last().unwrap().size;
+        if s > max_size {
+            max_size = s;
+        }
+    }
+    // From this data, we pack blobs into the image
+    println!("Packing blobs");
+    let blob_max_size = 80.;
+    let blob_min_size = 2.;
+    let [w, h] = texture.size();
+    let win = geom::Rect::from_w_h(w as f32, h as f32);
+
+    let mut sorted_files: Vec<FileHistory> = file_history.values().map(|fh| fh.clone()).collect();
+    sorted_files.sort_by_cached_key(|fh| fh.changes.last().unwrap().size);
+    let mut blobs: Vec<Blob> = Vec::with_capacity(sorted_files.len());
+    for fh in sorted_files.into_iter().rev() {
+        // Try a random position and check that it isn't inside a circle and that it doesn't overlap a different circle
+        let mut pt;
+        let mut radius;
+        loop {
+            pt = pt2(random_f32() * win.w(), random_f32() * win.h());
+            radius = (fh.changes.last().unwrap().size as f32 / max_size as f32).powf(0.266)
+                * blob_max_size
+                + blob_min_size;
+            let mut failed = false;
+            for b in &blobs {
+                if b.overlaps(pt, radius) {
+                    failed = true;
+                    break;
+                }
+            }
+            if !failed {
+                break;
+            }
+        }
+
+        let hue = if let Some(ext) = PathBuf::from(fh.path).extension() {
+            (ext.to_str().unwrap().as_bytes().iter().map(|b| *b as u32).sum::<u32>() % 256) as f32 / 256.
+        } else {
+            0.0
+        };
+        blobs.push(Blob {
+            pos: pt,
+            radius,
+            color: hsla(
+                0.5 + hue * 0.15,
+                0.5 + hue * 0.5,
+                ((fh.start_timestamp - first_timestamp) as f64 / (last_timestamp - first_timestamp) as f64) as f32 * 0.45 + 0.15,
+                1.0,
+            ),
+        })
+    }
+
+    for (author, _index) in author_indices {
+        println!("Author: {}", author);
+    }
+
     Model {
+        first_timestamp,
+        last_timestamp,
         commits,
+        author_history,
+        file_history,
         draw,
         texture_capturer,
         texture_reshaper,
         texture,
         renderer,
         screenshot: false,
+        zoom: 10.,
+        focus_point: pt2(0., 0.),
+        blobs,
     }
 }
 
@@ -132,25 +299,76 @@ fn update(app: &App, model: &mut Model, _update: Update) {
         //
         draw.background().color(BLACK);
         let num_commits = model.commits.len();
-        for (i, c) in model.commits.iter().enumerate() {
-            let x = (win.w() / num_commits as f32) * i as f32 + win.left();
-            let y = (c.impact as f32 / 10000.).clamp(-1.0, 1.0);
-            let ysign = y < 0.;
-            let y = y.abs().powf(0.33) * win.bottom() * 0.8;
-            let y = if ysign { y * -1. } else { y };
-            let size = (c.files_changed.len() as f32 / 10000.)
-                .clamp(0.0, 1.0)
-                .powf(0.5)
-                * win.h()
-                * 0.25
-                + 2.;
-            let r = (c.insertions as f32 / 1000.).powf(0.25);
-            let b = (c.deletions as f32 / 1000.).powf(0.25);
+        let time_width = model.last_timestamp - model.first_timestamp;
+
+        // Draw commits over time
+        // for (i, c) in model.commits.iter().enumerate() {
+        //     let x = (win.w() / num_commits as f32) * i as f32 + win.left();
+        //     let y = (c.impact as f32 / 10000.).clamp(-1.0, 1.0);
+        //     let ysign = y < 0.;
+        //     let y = y.abs().powf(0.33) * win.bottom() * 0.8;
+        //     let y = if ysign { y * -1. } else { y };
+        //     let size = (c.files_changed.len() as f32 / 10000.)
+        //         .clamp(0.0, 1.0)
+        //         .powf(0.5)
+        //         * win.h()
+        //         * 0.25
+        //         + 2.;
+        //     let r = (c.insertions as f32 / 1000.).powf(0.25);
+        //     let b = (c.deletions as f32 / 1000.).powf(0.25);
+        //     draw.ellipse()
+        //         .x_y(x, y)
+        //         .w_h(size, size)
+        //         .color(rgba(r, 0., b, 0.1));
+        // }
+
+        // Draw file history
+        // let num_files = model.file_history.len() as f64;
+        // let num_files_usize = model.file_history.len();
+        // let x_step = win.w() / num_commits as f32;
+        // let x_ts_step = win.w() * model.zoom / time_width as f32;
+        // for (i, fh) in model.file_history.values().enumerate() {
+        //     let start_x = x_step * fh.start_commit_index as f32 + win.left();
+        //     let file_x_step = ((fh.end_commit_index - fh.start_commit_index) as f32 * x_step)
+        //         / fh.changes.len() as f32;
+        //     let mut points_up = Vec::with_capacity(fh.changes.len());
+        //     let mut points_down = Vec::with_capacity(fh.changes.len());
+        //     let y = -(win.h()/2.) + ((i * 10 % num_files_usize) as f64 / num_files) as f32 * win.h();
+        //     for (j, change) in fh.changes.iter().enumerate() {
+        //         // let y = if i % 2 == 0 { y  } else { y * -1. };
+        //         // let x = start_x + file_x_step * j as f32;
+        //         // x value from timestamp
+        //         let x = x_ts_step * (change.timestamp - model.first_timestamp) as f32 + win.left();
+        //         let size = change.size;
+        //         let size = (size as f32 / 100000.).clamp(0., 1.0).powf(0.25) * win.w() * 0.02 + 0.5;
+        //         let col = hsla(change.author as f32 / 30.0, 0.8, 0.3, 1.0);
+        //         // draw.ellipse()
+        //         //     .x_y(x, y)
+        //         //     .radius(size)
+        //         //     .color(col);
+        //         points_up.push(pt2(x, y + size));
+        //         points_down.push(pt2(x, y - size));
+        //     }
+        //     let col = hsla(i as f32 * 300.5823723 % 1.0, 0.8, 0.3, 0.05);
+        //     // draw.polyline().color(col).points(points_up);
+        //     // draw.polyline().color(col).points(points_down);
+        //     let mut polygon_points = Vec::with_capacity(points_up.len() * 2);
+        //     for p in points_up.into_iter().chain(points_down.into_iter().rev()) {
+        //         polygon_points.push(p);
+        //     }
+        //     draw.polygon().color(col).points(polygon_points);
+        // }
+
+        // Draw files as blobs
+        for b in &model.blobs {
             draw.ellipse()
-                .x_y(x, y)
-                .w_h(size, size)
-                .color(rgba(r, 0., b, 0.1));
+                .color(b.color)
+                .xy(b.pos - (win.wh() / 2.))
+                .w_h(b.radius * 2., b.radius * 2.);
         }
+
+        // List of files with sizes
+        // Circle packing
 
         // Render our drawing to the texture.
         model
@@ -193,7 +411,7 @@ fn view(_app: &App, model: &Model, frame: Frame) {
         .encode_render_pass(frame.texture_view(), &mut *encoder);
 }
 
-fn window_event(_app: &App, model: &mut Model, event: WindowEvent) {
+fn window_event(app: &App, model: &mut Model, event: WindowEvent) {
     match event {
         KeyPressed(key) => match key {
             Key::A => {}
@@ -217,10 +435,17 @@ fn window_event(_app: &App, model: &mut Model, event: WindowEvent) {
             Key::R => {}
             Key::G => {}
             Key::Space => {}
+            Key::Plus => {
+                model.zoom += 1.;
+            }
+            Key::Minus => {
+                model.zoom -= 1.;
+            }
             _ => (),
         },
         KeyReleased(_key) => {}
         MouseMoved(pos) => {
+            model.focus_point = pt2(pos.x / app.window_rect().w(), pos.y / app.window_rect().h());
             // model.param1 = (pos.x + app.window_rect().w() / 2.0) / app.window_rect().w();
         }
         MousePressed(button) => match button {
