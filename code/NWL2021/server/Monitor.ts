2eff80f3e12eac6151867c79157599979a048015
tdurieux <durieuxthomas@hotmail.com>
2021-10-19 11:57:59 +0200

    feat: add async monitor

--- code/NWL2021/server/Monitor.ts
+++ code/NWL2021/server/Monitor.ts
@@ -2,6 +2,8 @@ import { Server, Socket } from "socket.io";
 import { event as dbEvent } from "./database/database";
 import { MonitoringEvent } from "./types";
 
+import asyncHooks from "async_hooks";
+
 export default class Monitor {
   constructor(readonly io: Server) {
     io.of("component").on("connection", (socket) =>
@@ -11,6 +13,18 @@ export default class Monitor {
       this._visualizationConnect(socket)
     );
     dbEvent.subscribe((event) => this.send(event));
+
+    const monitor = this;
+    const asyncHook = asyncHooks.createHook({
+      init(asyncId, type, triggerAsyncId) {
+        if (type != "TickObject")
+          monitor.send({
+            origin: "node",
+            action: "async init " + type,
+          });
+      },
+    });
+    asyncHook.enable();
   }
 
   private _componentConnect(socket: Socket): void {
