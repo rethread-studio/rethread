8ff0c7ec93d35edac813fa3c26d5155b73656fa9
imjhonny <ramjonathan@gmail.com>
2021-11-03 09:50:02 +0100

    add demo, question and results screen

--- code/NWL2021/screen/public/script/index.js
+++ code/NWL2021/screen/public/script/index.js
@@ -314,11 +314,26 @@ function drawAnswers(question) {
   }
 }
 
-function hideAnswers() {
+function showAnswers(_show) {
   const answerE1 = document.querySelector(".answer1");
   const answerE2 = document.querySelector(".answer2");
-  answerE1.style.visibility = 'hidden';
-  answerE2.style.visibility = 'hidden';
+  answerE1.style.display = _show ? 'block' : 'none';
+  answerE2.style.display = _show ? 'block' : 'none';
+}
+
+function showResults(_show) {
+  const results = document.querySelector(".result");
+  results.style.display = _show ? 'block' : 'none';
+}
+
+function showQuestion(_show) {
+  const results = document.querySelector(".question");
+  results.style.display = _show ? 'block' : 'none';
+}
+
+function showDemo(_show) {
+  const results = document.querySelector(".demo");
+  results.style.display = _show ? 'flex' : 'none';
 }
 
 function getCBoardClass() {
@@ -335,9 +350,10 @@ setInterval(() => {
   gameCycle = !gameCycle;
 }, 1000);
 
-setInterval(() => {
+window.requestAnimationFrame(gameLoop);
+function gameLoop() {
   updateGameState();
-}, 1000);
+}
 
 function updateGameState() {
   if (!gameState) gameState = {};
@@ -345,34 +361,77 @@ function updateGameState() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   //render status
   // const status = gameState.status;
-  const status = "question";
+  const status = "play";
   switch (status) {
     case "demo":
+      showResults(false);
+      showAnswers(false);
+      showQuestion(false);
+      showDemo(true);
       renderDemo();
       break;
     case "play":
+      showAnswers(true);
+      showDemo(false);
       renderGame();
       break;
     case "question":
-      hideAnswers();
+      showAnswers(false);
+      showResults(false);
+      showQuestion(true);
       drawQuestion(gameState.question);
       break;
     case "result":
+      showAnswers(false);
+      showResults(true);
       renderResult();
       break;
     default:
+      showResults(false);
+      showAnswers(false);
+      showQuestion(false);
       renderDemo();
       break;
   }
 }
 
+const mapPlayerToHtmlImg = (p, i) => {
+  const roation = gameCycle ? i % 2 == 0 ? 45 : -45 : i % 2 == 0 ? -45 : 45;
+  return `<div class="player">
+      <img style="transform: rotate(${roation}deg);" src="${"/img/laureates/" + p.laureate.imagePath}" alt="${p.laureate.firstname}">
+    </div>`;
+}
+
+function filterAnswer(_correct) {
+  return (p) => p.inAnswer == _correct;
+}
+
 function renderResult() {
-  console.log("render result")
+  if (gameState.question == undefined) return;
+  const question = document.querySelector(".result .q");
+  const answer = document.querySelector(".result .a");
+  question.innerHTML = gameState.question.text;
+  answer.innerHTML = gameState.question.answers.find((a => a.isCorrect == true)).text;
+
+  const correctPlayers = gameState.players
+    .filter(filterAnswer(true))
+    .map(mapPlayerToHtmlImg)
+    .join(" ");
+  const wrongPlayers = gameState.players
+    .filter(filterAnswer(false))
+    .map(mapPlayerToHtmlImg)
+    .join(" ");
+
+  const correctGroup = document.querySelector(".correct");
+  const wrongGroup = document.querySelector(".wrong");
+
+  correctGroup.innerHTML = correctPlayers;
+  wrongGroup.innerHTML = wrongPlayers;
 }
 
 
 function renderDemo() {
-  console.log("render demo")
+
 }
 
 function renderGame() {
@@ -391,4 +450,9 @@ function start(s) {
   document.querySelector(
     ".board"
   ).style = `width: ${canvas.width}px;height:${canvas.height}px;`;
+
+  showAnswers(false);
+  showResults(false);
+  showQuestion(false);
+  showDemo(false);
 }
