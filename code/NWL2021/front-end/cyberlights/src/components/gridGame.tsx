6fb81f0abff7d6052f1a01a6861abc671d7efdf3
imjhonny <ramjonathan@gmail.com>
2021-11-12 10:35:20 +0100

    fix controllerGrid width

--- code/NWL2021/front-end/cyberlights/src/components/gridGame.tsx
+++ code/NWL2021/front-end/cyberlights/src/components/gridGame.tsx
@@ -8,13 +8,15 @@ interface IGridGame {
 
 export const GridGame = ({ state, position }: React.PropsWithChildren<IGridGame>) => {
 
+    const tileSize: number = (window.innerWidth - 16 * 2) / state.width;
+
     let grid = <></>;
     for (let i = 0; i < (state?.height || 0); i++) {
         let line = <></>;
         for (let j = 0; j < state.width; j++) {
             line = <>
                 {line}
-                <div className={"grid-position-item " + ((position.y === i && position.x === j) ? "active " : " ") + (isWall(j, i, state) ? "wall " : " ") + (isAnswer(j, i, state) ? "grid-answer " : " ")}></div>
+                <div style={{ width: `${tileSize}px`, height: `${tileSize}px` }} className={"grid-position-item " + ((position.y === i && position.x === j) ? "active " : " ") + (isWall(j, i, state) ? "wall " : " ") + (isAnswer(j, i, state) ? "grid-answer " : " ")}></div>
             </>
         }
         grid = <>{grid}<div className="grid-line">{line}</div></>
