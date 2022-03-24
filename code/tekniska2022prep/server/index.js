873172e8f1e5768e486597d03bb21321f0bb9895
tdurieux <durieuxthomas@hotmail.com>
2022-03-24 12:42:09 +0100

    feat: add controler page + mosaic image

--- code/tekniska2022prep/server/index.js
+++ code/tekniska2022prep/server/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
+const fs = require("fs");
+const mosaic = require("mosaic-node-generator");
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-
 const osc = require("./osc");
+
+const snapsFolder = __dirname + "/snaps/";
 
 osc.open((port) => {
   console.log("OSC server listening on port " + port);
@@ -17,6 +20,54 @@ io.emit = function () {
 };
 
 app.use(express.static(__dirname + "/../ui_demo"));
+
+async function generateMosaic() {
+  console.log("Generating mosaic...");
+  const m = new mosaic.MosaicImage(
+    new mosaic.JimpImage(
+      await mosaic.JimpImage.read(__dirname + "/img/idle.jpeg")
+    ),
+    snapsFolder,
+    mosaic.CONFIG.cell_width,
+    mosaic.CONFIG.cell_height,
+    10,
+    10,
+    __dirname + "/thumbs",
+    __dirname + "/thumbs",
+    false
+  );
+  await m.readTiles();
+  await m.processRowsAndColumns(0, 0, m.rows, m.columns);
+  await m.image.save(__dirname + "/img/mosaic");
+  // m.generateThumbs();
+}
+
+setInterval(generateMosaic, 3 * 60 * 1000);
+try {
+  generateMosaic();
+} catch (error) {
+  console.error(error);
+}
+
+app.get("/img/mosaic.jpg", async (req, res) => {
+  if (fs.existsSync(__dirname + "/img/mosaic.jpg")) {
+    res.sendFile(__dirname + "/img/mosaic.jpg");
+  } else {
+    res.sendFile(__dirname + "/img/idle.jpeg");
+  }
+});
+
+app.get("/js/button_map.js", (req, res) => {
+  res.send(
+    "button_map=" +
+      JSON.stringify({
+        RESET_BUTTON: process.env.RESET_BUTTON,
+        SPEED1_BUTTON: process.env.SPEED1_BUTTON,
+        SPEED2_BUTTON: process.env.SPEED2_BUTTON,
+        SPEED3_BUTTON: process.env.SPEED3_BUTTON,
+      })
+  );
+});
 
 const buttonMap = {};
 buttonMap[process.env.RESET_BUTTON] = "RESET_BUTTON";
@@ -35,6 +86,29 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("a client disconnected");
+  });
+
+  socket.on("picture", (image) => {
+    const fileName = "snap_" + new Date().getTime();
+    const filePath = snapsFolder + fileName + ".png";
+    // Remove header
+    let base64Image = image.split(";base64,").pop();
+    fs.writeFile(
+      filePath,
+      base64Image,
+      { encoding: "base64" },
+      async function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          const img = new mosaic.JimpImage(
+            await mosaic.JimpImage.read(filePath)
+          );
+          img.image.cover(mosaic.CONFIG.cell_width, mosaic.CONFIG.cell_height);
+          await img.save(__dirname + "/thumbs/" + fileName);
+        }
+      }
+    );
   });
 
   socket.on("serial", (data) => {
