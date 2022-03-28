e16cd21d20f64e806d798534ef94a4be81224142
imjhonny <ramjonathan@gmail.com>
2022-03-28 11:16:56 +0200

    add speed visualizations

--- code/tekniska2022prep/ui_demo/js/app.js
+++ code/tekniska2022prep/ui_demo/js/app.js
@@ -6,12 +6,15 @@ socket.on("state", (state) => {
   // RESET_BUTTON_OFF, SPEED1_BUTTON_OFF, SPEED2_BUTTON_OFF, SPEED3_BUTTON_OFF
   document.querySelector(".current-state").innerHTML = state;
   document.querySelector(".idle").style.display = "none";
+
+
   if (state == "PICTURE") {
     const image = webcam.snap();
     socket.emit("picture", image);
     document.querySelector(".camera").style.display = "none";
     document.querySelector(".snap img").style.display = "block";
     document.querySelector(".snap img").src = image;
+    loadNewImage(image);
   } else if (state == "RESET_BUTTON_ON") {
     document.querySelector(".camera").style.display = "block";
     document.querySelector(".snap img").style.display = "none";
@@ -19,6 +22,15 @@ socket.on("state", (state) => {
     document.querySelector(".idle").style.display = "block";
     document.querySelector(".camera").style.display = "none";
     document.querySelector(".snap img").style.display = "none";
+  } else if (state == "SPEED1_BUTTON_ON") {
+    tunOnSpeed(state)
+
+  } else if (state == "SPEED2_BUTTON_ON") {
+    tunOnSpeed(state)
+
+  } else if (state == "SPEED3_BUTTON_ON") {
+    tunOnSpeed(state)
+
   }
 });
 window.addEventListener(
@@ -28,3 +40,67 @@ window.addEventListener(
   },
   false
 );
+
+
+
+function preload() {
+  photograph = loadImage('./img/portrait100.jpg');
+}
+
+function setup() {
+
+  canvas = createCanvas(windowWidth, windowHeight);
+  frameRate(state.frameRate);
+
+  filters.push(
+    { filter: ERODE, val: [1] },
+    { filter: GRAY, val: [1] },
+    { filter: BLUR, val: [0.9] },
+  );
+
+  objectsToRender = [];
+  speed1 = {};
+  speed2 = {};
+
+  images = new Images(photograph, filters);
+  filter = createFilter(images.getImages());
+
+  objectsToRender.push(filter);
+
+  speed1.render = () => {
+    images.renderFirstAndLastImage();
+  }
+
+  speed2.render = () => {
+    images.renderFirstImage();
+  }
+
+}
+
+function draw() {
+  background(state.backgroundCol);
+  update();
+  render();
+}
+
+function tunOnSpeed(speed) {
+  emptyObjectsToRender();
+  emptyParticles();
+  removeInterval();
+  switch (speed) {
+    case 'SPEED1_BUTTON_ON':
+      objectsToRender.push(speed1);
+      break;
+    case 'SPEED2_BUTTON_ON':
+      createPixelParticles(images.getFirstImage(), images.getLastImage())
+      objectsToRender.push(speed2);
+      break;
+    case 'SPEED3_BUTTON_ON':
+      objectsToRender.push(filter);
+      createInterval();
+      break;
+
+    default:
+      break;
+  }
+}
---
+++ code/tekniska2022prep/ui_demo/js/src/Image/Images.js
@@ -0,0 +1,65 @@
+
+class Images {
+    images = [];
+    filters = [];
+
+    constructor(img, filters) {
+        this.filters = filters;
+        this.setImages(img);
+    }
+
+    getImages() {
+        return this.images;
+    }
+
+    setImages(img) {
+        const size = { width: img.width, height: img.height };
+        const pixelImage = this.createFilledImage(img, { x: 100, y: img.height }, size, pixelSampleSize);
+        this.images.push(pixelImage);
+        let imgRef = pixelImage;
+        this.filters.forEach(_filter => {
+            const pos = imgRef.getPosition();
+            const emptyImg = this.createEmptyImage({ x: pos.x + size.width + size.width, y: pos.y }, size, pixelSampleSize, imgRef.getCompleteImage(), _filter);
+            imgRef = emptyImg;
+            this.images.push(emptyImg);
+        });
+    }
+
+    getFirstImage() {
+        return this.images[0];
+    }
+
+    getLastImage() {
+        return this.images[this.images.length - 1];
+    }
+
+    createEmptyImage(position, size, sampleSize, _sampleImage, filter) {
+        const emptyImage = new ImageSample();
+        emptyImage.setPosition(position.x, position.y);
+        emptyImage.setImage(null, size.width, size.height);
+        emptyImage.setIsSqueletonVisible(true);
+        emptyImage.setSampleSize(sampleSize.width, sampleSize.height);
+        emptyImage.setCompleteImage(_sampleImage, size.width, size.height);
+        emptyImage.filterImage(filter);
+        return emptyImage;
+    }
+
+    createFilledImage(_image, position, size, sampleSize) {
+        const imageOut = new ImageSample();
+        imageOut.setPosition(position.x, position.y);
+        imageOut.setImage(_image, size.width, size.height);
+        imageOut.setCompleteImage(_image, size.width, size.height);
+        imageOut.setSampleSize(sampleSize.width, pixelSampleSize.height);
+        imageOut.setLoadedPos(size.width, size.height);
+        return imageOut;
+    }
+
+    renderFirstAndLastImage() {
+        this.images[0].renderCompleteImage();
+        this.images[this.images.length - 1].renderCompleteImage();
+    }
+
+    renderFirstImage() {
+        this.images[0].renderCompleteImage();
+    }
+}
