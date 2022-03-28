326a3facc97ea8ca97766398dee438fb5ef21da4
tdurieux <durieuxthomas@hotmail.com>
2022-03-28 11:59:09 +0200

    feat: add rotary support

--- code/tekniska2022prep/serial_server/index.js
+++ code/tekniska2022prep/serial_server/index.js
@@ -1,7 +1,34 @@
 require("dotenv").config();
+const Gpio = require("pigpio").Gpio;
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
 var io = require("socket.io-client");
+
+const button = new Gpio(27, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+});
+
+const clock = new Gpio(17, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+});
+
+const direction = new Gpio(18, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+});
+
+direction.on("interrupt", (level) => {
+  console.log("direction", level);
+});
+
+clock.on("interrupt", (level) => {
+  console.log("clock", level);
+});
+button.on("interrupt", (level) => {
+  console.log("button", level);
+});
 
 const port = new SerialPort({
   path: process.env.TTY,
@@ -23,7 +50,6 @@ socket = io.connect(process.env.WS_URL, {
 socket.on("connect", function () {
   console.log("socket connected to " + process.env.WS_URL);
   socket.emit("hello", "world");
-
 });
 
 setInterval(() => {
