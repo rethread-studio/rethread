bb6d00de7180c99e9571adfd43c67238352f6217
tdurieux <durieuxthomas@hotmail.com>
2022-04-11 11:59:51 +0200

    fix: change rotation speed precision to 100milli

--- code/tekniska2022prep/server/index.js
+++ code/tekniska2022prep/server/index.js
@@ -86,19 +86,19 @@ const buttonState = {};
 
 const countEvent = {};
 function countEventSec() {
-  const date = new Date().getTime() / 1000;
+  const date = new Date().getTime() / 100;
   countEvent[date] = countEvent[date] ? countEvent[date] + 1 : 1;
 }
 
 /**
- * return the average number of events in the last 5 minutes
+ * return the average number of events in the last 500 milliseconds
  * @returns {number}
  */
 function getNbEventSec() {
   let total = 0;
   for (const i in countEvent) {
-    if (new Date().getTime() / 1000 - i >= 5) {
-      // keep data from the last 5 sec
+    if (new Date().getTime() / 100 - i >= 5) {
+      // keep data from the last 500 milliseconds
       delete countEvent[i];
       continue;
     }
@@ -144,12 +144,13 @@ io.on("connection", (socket) => {
 
   socket.on("rotary", (data) => {
     countEventSec();
+    const speed = getNbEventSec();
     if (data.direction == "R") {
-      io.emit("step", "next");
-      osc.send({state: "next", events: getNbEventSec()});
+      io.emit("step", { direction: "next", speed });
+      osc.send({ state: "next", events: speed });
     } else {
-      io.emit("step", "previous");
-      osc.send({state: "previous", events: getNbEventSec()});
+      io.emit("step", { direction: "previous", speed });
+      osc.send({ state: "previous", events: speed });
     }
   });
 
@@ -191,7 +192,7 @@ function setState(s) {
   clearTimeout(resetTimeout);
   state = s;
   io.emit("state", state);
-  osc.send({state, events: getNbEventSec()});
+  osc.send({ state, events: getNbEventSec() });
   if (state !== "IDLE") {
     resetIdle();
   }
