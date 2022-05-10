f7f239f3cb254fdd7bdb490889e539e1411793ab
tdurieux <durieuxthomas@hotmail.com>
2022-05-10 17:01:47 +0200

    improve serial server

--- code/tekniska2022prep/serial_server/index.js
+++ code/tekniska2022prep/serial_server/index.js
@@ -23,30 +23,40 @@ myEncoder.on("rotation", (direction, value) => {
   previousValue = value;
 });
 
-const port = new SerialPort({
-  path: process.env.TTY,
-  baudRate: 9600,
-  autoOpen: true,
-});
-port.on("open", () => {
-  console.log("Connected to serial port: " + process.env.TTY);
-});
-let lastData;
-const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
-parser.on("data", (data) => {
-  lastData = data;
-});
-
-socket = io.connect(process.env.WS_URL, {
-  reconnect: true,
-});
-socket.on("connect", function () {
-  console.log("socket connected to " + process.env.WS_URL);
-  socket.emit("hello", "world");
-});
-
-setInterval(() => {
-  socket.emit("serial", lastData, (response) => {
-    console.log(response); // "got it"
+if (process.env.TTY) {
+  const port = new SerialPort({
+    path: process.env.TTY,
+    baudRate: 9600,
+    autoOpen: true,
   });
-}, 100);
+  port.on("open", () => {
+    console.log("Connected to serial port: " + process.env.TTY);
+  });
+  let lastData;
+  const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
+  parser.on("data", (data) => {
+    lastData = data;
+  });
+} else {
+  console.warn("TTY path not provided in the ENV (TTY)");
+}
+
+if (process.env.WS_URL) {
+  socket = io.connect(process.env.WS_URL, {
+    reconnect: true,
+  });
+  socket.on("connect", function () {
+    console.log("socket connected to " + process.env.WS_URL);
+    socket.emit("hello", "world");
+  });
+
+  setInterval(() => {
+    if (lastData) {
+      socket.emit("serial", lastData, (response) => {
+        console.log(response); // "got it"
+      });
+    }
+  }, 100);
+} else {
+  console.warn("WS_URLnot provided");
+}
