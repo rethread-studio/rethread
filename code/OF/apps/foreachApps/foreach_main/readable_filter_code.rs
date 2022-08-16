7d849b65325cd8f3360dfd1c8ecc473125eaac3e
Erik Natanael Gustafsson <erik@eriknatanael.com>
2022-08-16 13:23:36 +0200

    Human readable version of the filter code

---
+++ code/OF/apps/foreachApps/foreach_main/readable_filter_code.rs
@@ -0,0 +1,33 @@
+
+fn apply_filter_to_pixel(coordinate: Position) {
+  Color original_color =  image[coordinate].rgb;
+
+  let brightness = luma(orignal_color);
+  Color purple = rgb(1.0, 0.2, 0.7);
+  Color blue = rgb(0.1, 0.7, 1.2);
+
+  let linear_brightness = (luma + gain).pow(exponent);
+  Color new_color = mix(blue, purple, smooth_curve(0.1, 1.0, linear_brightness));
+  new_color = mix(org_color, new_color, smooth_curve(0.0, 0.1, linear_brightness));
+  new_color = mix(color, purple, smooth_curve(0.7, 1.0, linear_brightness));
+
+  new_color *= (luma + 0.1).pow(1.5) + 0.1;
+    image[coordinate] = new_color;
+}
+
+
+for each pixel in image {
+  Color original_color =  image[pixel].rgb;
+
+  let brightness = luma(orignal_color);
+  Color purple = rgb(1.0, 0.2, 0.7);
+  Color blue = rgb(0.1, 0.7, 1.2);
+
+  let linear_brightness = (luma + gain).pow(exponent);
+  Color new_color = mix(blue, purple, smooth_curve(0.1, 1.0, linear_brightness));
+  new_color = mix(org_color, new_color, smooth_curve(0.0, 0.1, linear_brightness));
+  new_color = mix(color, purple, smooth_curve(0.7, 1.0, linear_brightness));
+
+  new_color *= (luma + 0.1).pow(1.5) + 0.1;
+    image[pixel] = new_color;
+}
