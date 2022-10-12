cf5cb8921269a1a8cbcffba66a022786de144bc9
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2022-10-12 12:22:54 +0200

    add two more visualizations + better code organization

--- code/NWL2022/LED_web_demo/sketch.js
+++ code/NWL2022/LED_web_demo/sketch.js
@@ -1,3 +1,5 @@
+// Global variables 🌐
+
 const Nx = 5; // number of LEDs in the x-direction
 const Ny = 15; // number of LEDs in the y-direction
 const diam = 5; // diameter of each sphere (LED)
@@ -13,25 +15,30 @@ let nDeps; // number of dependencies
 let nSups; // number of suppliers
 
 let t = 0; // time
-let ledsLeft = [], ledsRight = []; // leds on the left, and on the right. Both are
+let ledsLeft = [], ledsRight = []; // LEDs on the left, and on the right. Both are 2D-arrays of size Ny*Nx
 
-let speedSlider; // animation speed
-let sepSlider; // separation between the two curtains
-let angleSlider; // angle they make with the camera plane
 
-// GUI
+
+// GUI 🖱️
+
 let params = {
   speed: 3,
   separation: 0,
   angle: Math.PI/4,
-  vizStyle: 0
+  hueGradient: 5,
+  vizStyle: "In/out"
 }
 
 let gui = new dat.GUI();
 gui.add(params, "speed", 1, 20, 1).name("Slowness")
-gui.add(params, "separation", 0, 64, 1).name("Separation");
-gui.add(params, "angle", 0, Math.PI, Math.PI/16).name("Angle");
-gui.add(params, "vizStyle", [0, 1, 2]).name("Style");
+gui.add(params, "separation", 0, 100, 1).name("Separation");
+gui.add(params, "angle", 0, 2*Math.PI, Math.PI/16).name("Angle");
+gui.add(params, "hueGradient", 0, 360/Ny, 1).name("Hue gradient");
+gui.add(params, "vizStyle", ["In/out", "Right/left", "Up/down"]).name("Style");
+
+
+
+// p5.js 🎨
 
 function preload() {
   //data = loadJSON("data-varna-copy-paste-isolated.json");
@@ -43,10 +50,9 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500, 500, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
   colorMode(HSB);
-  //frameRate(10)
   //ortho();
 
   trace = data.draw_trace;
@@ -79,13 +85,14 @@ function draw() {
 
   orbitControl(); // allow movement around the sketch using a mouse or trackpad
   lights(); // add global lights to the scene
-  translate(-Nx*s/2, -Ny*s/2);
+  translate(-Nx*s/2, -Ny*s/2); // center the LEDs
 
   if (frameCount % params.speed == 0) {
     updateLeds();
     t++;
   }
 
+  // Left LEDs
   push();
   translate(-params.separation, 0, 0);
   rotateY(params.angle);
@@ -93,6 +100,7 @@ function draw() {
   drawLEDs(ledsLeft);
   pop();
 
+  // Right LEDs
   push();
   translate(Nx*s+params.separation, 0, 0);
   rotateY(-params.angle);
@@ -102,18 +110,65 @@ function draw() {
 }
 
 function updateLeds() {
-  for (let i = 0; i < Nx; i++) {
-    let tr = trace[(i+t)%len];
-    let [sup, dep] = getSupAndDep(tr.name);
-    let depth = ~~(tr.depth*Ny/(maxDepth+1));
-    for (let j = 0; j < Ny; j++) {
-      let huLeft = allDeps.indexOf(dep)*360/allDeps.length;;
-      let huRight = allSups.indexOf(sup)*360/allSups.length;
-      let sa = 100;
-      let br = (j < depth) ? 100 : 0;
-      ledsLeft[j][i] = [huLeft, sa, br];
-      ledsRight[j][Nx-i-1] = [huRight, sa, br];
-    }
+  switch (params.vizStyle) {
+
+    case "In/out":
+      for (let i = 0; i < Nx; i++) {
+        let tr = trace[(i+t)%len];
+        let [sup, dep] = getSupAndDep(tr.name);
+        let depth = ~~(tr.depth*Ny/(maxDepth+1));
+        for (let j = 0; j < Ny; j++) {
+          let huLeft = (allDeps.indexOf(dep)*360/allDeps.length + j*params.hueGradient)%360;
+          let huRight = (allSups.indexOf(sup)*360/allSups.length + j*params.hueGradient)%360;
+          let sa = 100;
+          let br = (j < depth) ? 100 : 0;
+          ledsLeft[j][i] = [huLeft, sa, br];
+          ledsRight[j][Nx-i-1] = [huRight, sa, br];
+        }
+      }
+      break;
+
+    case "Right/left":
+      for (let i = 0; i < Nx; i++) {
+        let tr = trace[abs(i-t)%len];
+        let [sup, dep] = getSupAndDep(tr.name);
+        let depth = ~~(tr.depth*Ny/(maxDepth+1));
+        for (let j = 0; j < Ny; j++) {
+          let huLeft = (allDeps.indexOf(dep)*360/allDeps.length + j*params.hueGradient)%360;
+          let sa = 100;
+          let br = (j < depth) ? 100 : 0;
+          ledsLeft[j][i] = [huLeft, sa, br];
+        }
+      }
+      for (let i = 0; i < Nx; i++) {
+        let tr = trace[abs(i-t+Nx)%len];
+        let [sup, dep] = getSupAndDep(tr.name);
+        let depth = ~~(tr.depth*Ny/(maxDepth+1));
+        for (let j = 0; j < Ny; j++) {
+          let huRight = (allSups.indexOf(sup)*360/allSups.length + j*params.hueGradient)%360;
+          let sa = 100;
+          let br = (j <= depth) ? 100 : 0;
+          ledsRight[j][i] = [huRight, sa, br];
+        }
+      }
+      break;
+
+    case "Up/down":
+      for (let j = 0; j < Ny; j++) {
+        let tr = trace[abs(j+t)%len];
+        let [sup, dep] = getSupAndDep(tr.name);
+        let depth = ~~(tr.depth*Nx/(maxDepth+1));
+        for (let i = 0; i < Nx; i++) {
+          let huLeft = (allDeps.indexOf(dep)*360/allDeps.length + i*params.hueGradient)%360;
+          let huRight = (allSups.indexOf(sup)*360/allSups.length + i*params.hueGradient)%360;
+          let sa = 100;
+          let br = (i <= depth) ? 100 : 0;
+          ledsLeft[j][Nx-i-1] = [huLeft, sa, br];
+          ledsRight[j][i] = [huRight, sa, br];
+        }
+      }
+      break;
+
   }
 }
 
@@ -136,7 +191,7 @@ function drawLEDs(leds) {
 
 
 
-// data analysis 🤓
+// data analysis 🧮
 
 function getLength() {
   // gets the lengths of data and stores it in variable "len"
