433dfcb63bab9292aa7a01f21b9c166c5ae87a94
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2022-10-20 15:36:15 +0200

    first window projection demo

---
+++ code/NWL2022/window_projections/helix/sketch.js
@@ -0,0 +1,136 @@
+let data;
+let trace_len; // length of trace
+let w, h; // width and height of each rectangle
+let hGap; // gap between rectangles
+let wMargin = 10; // margin all around
+
+const SCALE = 2;
+const WINDOW_WIDTH = 57*SCALE;
+const WINDOW_HEIGHT = 112*SCALE;
+let mWindows = 1; // how many windows in width
+let nWindows = 2; // how many windows in height
+let showWindowFrame = true;
+
+let allDeps; // array with all dependencies
+let allSups; // array with all suppliers
+let nDeps; // number of dependencies
+let nSups; // number of suppliers
+
+let cnv, ctx; // canvas and its context
+
+function preload() {
+  //data = loadJSON("../../LED_web_demo/data-imagej-copy-paste_parsed.json");
+  //data = loadJSON("../../LED_web_demo/data-varna-startup-shutdown_parsed.json");
+  data = loadJSON("../../LED_web_demo/data-varna-copy-paste-isolated_parsed.json");
+}
+
+function setup() {
+  cnv = createCanvas(WINDOW_WIDTH*mWindows, WINDOW_HEIGHT*nWindows, WEBGL);
+  noStroke();
+  colorMode(HSB);
+  ctx = canvas.getContext("2d");
+
+  trace_len = data.draw_trace.length;
+
+  initParams();
+
+  getAllSuppliersAndDependencies();
+  allDeps.sort();
+  allSups.sort();
+  nDeps = allDeps.length;
+  nSups = allSups.length;
+}
+
+function draw() {
+  background(0);
+  translate(-width/2, -height/2);
+
+  let x = wMargin, y = -(frameCount/2)%h-height/3, i = floor(frameCount/(2*h));
+  while (y < height+height/3) {
+    let d = data.draw_trace[(i++)%trace_len];
+    let [sup, dep] = getSupAndDep(d.name);
+    let hu1 = 360*allDeps.indexOf(dep)/allDeps.length;
+    let hu2 = 360*allSups.indexOf(sup)/allSups.length;
+    let sa = 90;
+    let va = 90;
+
+    push();
+    translate(x+w/2, y+h/2);
+    rotateY(frameCount/50 + y/50);
+    fill(hu1, sa, va);
+    beginShape();
+    vertex(-w/2+wMargin, -h/2+hGap/2);
+    vertex(-w/2+wMargin, h/2-hGap/2);
+    fill(hu2, sa, va);
+    vertex(w/2-wMargin, h/2-hGap/2);
+    vertex(w/2-wMargin, -h/2+hGap/2);
+    endShape();
+    pop();
+
+    y += h;
+  }
+
+  //console.log(frameRate());
+  if (showWindowFrame) drawWindowsOutline();
+}
+
+function initParams() {
+  // centering canvas
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+
+  w = width-wMargin*2;
+  h = 20;
+  hGap = h/8;
+}
+
+function drawWindowsOutline() {
+  stroke(255);
+  strokeWeight(2);
+  for (let x = WINDOW_WIDTH; x < width; x += WINDOW_WIDTH) {
+    line(x, 0, 0, x, height, 0);
+  }
+  for (let y = WINDOW_HEIGHT; y < height; y += WINDOW_HEIGHT) {
+    line(0, y, 0, width, y, 0);
+  }
+  noStroke();
+}
+
+function getSupAndDep(s) {
+  // s: a string corresponding to a method call, of the form "[eventual prefix]/[supplier].[dependency].[method name]"
+  // return: an array, the first value is the supplier, the second is the dependency
+  let func = (s.indexOf("/") == -1) ? s : s.split("/")[1]; // remove the eventual prefix, find the interesting part
+  let idx1 = func.indexOf(".", func.indexOf(".")+1); // find the second occurence of "."
+  let idx2 = func.indexOf(".", idx1+1); // find the first occurence of "." after idx1
+  let idx3 = func.indexOf("$", idx1+1); // find the first occurence of "$" after idx1
+  if (idx3 == -1) idx3 = idx2; // ignore idx3 if there is no "$" found
+  let supplier = func.slice(0, idx1); // find the supplier
+  let dependency = func.slice(idx1+1, min(idx2, idx3)); // find the dependency
+  return [supplier, dependency];
+}
+
+function getAllSuppliersAndDependencies() {
+  // find all suppliers, all dependencies, and save them in the global variables allSups and allDeps
+  allSups = [];
+  allDeps = [];
+  for (let i = 0; i < trace_len; i++) {
+    let t = data.draw_trace[i];
+    let [sup, dep] = getSupAndDep(t.name);
+    if (allSups.indexOf(sup) == -1) allSups.push(sup);
+    if (allDeps.indexOf(dep) == -1) allDeps.push(dep);
+  }
+}
+
+function keyPressed() {
+  if (key == " ") {
+    showWindowFrame = !showWindowFrame;
+    return;
+  }
+  if (keyCode == LEFT_ARROW && mWindows > 1) mWindows--;
+  if (keyCode == RIGHT_ARROW && mWindows < 14) mWindows++;
+  if (keyCode == DOWN_ARROW && nWindows > 1) nWindows--;
+  if (keyCode == UP_ARROW && nWindows < 5) nWindows++;
+  resizeCanvas(WINDOW_WIDTH*mWindows, WINDOW_HEIGHT*nWindows);
+  initParams();
+}
