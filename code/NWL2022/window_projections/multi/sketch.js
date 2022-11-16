641f19228dba5c4e6a7d47d22fca314e814c22dd
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2022-11-16 14:00:05 +0100

    add dataset + helix viz in multi

--- code/NWL2022/window_projections/multi/sketch.js
+++ code/NWL2022/window_projections/multi/sketch.js
@@ -1,12 +1,15 @@
 let data;
 let trace_len; // length of trace
 
+let keywords = ["copy", "paste", "search", "replace", "find", "write"];
 let dep_colors, sup_colors;
+let all_deps, all_sups;
 let myFont;
 
 let scale;
 const WINDOW_WIDTH = 57;
 const WINDOW_HEIGHT = 112;
+const SMOL_WINDOW_HEIGHT = 47;
 let mWindows = 3; // how many windows in width
 let nWindows = 1; // how many windows in height
 let showWindowFrame = true;
@@ -18,7 +21,7 @@ let zones = [];
 function preload() {
   //data = loadJSON("../../LED_web_demo/data-imagej-copy-paste_parsed.json");
   //data = loadJSON("../../LED_web_demo/data-varna-startup-shutdown_parsed.json");
-  data = loadJSON("../../LED_web_demo/data-varna-copy-paste-isolated_parsed.json");
+  data = loadJSON("../../LED_web_demo/data-jedit-with-marker.json");
   myFont = loadFont("../MPLUS1Code-VariableFont_wght.ttf");
   sup_colors = loadTable("../../color_tables/data-jedit-with-marker_supplier_colors.csv", "csv", "header");
   dep_colors = loadTable("../../color_tables/data-jedit-with-marker_dependency_colors.csv", "csv", "header");
@@ -27,13 +30,17 @@ function preload() {
 function setup() {
   determineScale();
   cnv = createCanvas(WINDOW_WIDTH*mWindows*scale, WINDOW_HEIGHT*nWindows*scale);
-  colorMode(HSB);
 
   //console.log(data)
   trace_len = data.draw_trace.length;
+  all_deps = dep_colors.getColumn(0);
+  all_sups = sup_colors.getColumn(0);
   centerCanvas();
 
   console.log("Instructions:\n- arrow keys to change window dimensions\n- space to show/hide window frames");
+
+  zones.push(helix(0, 0, 1, 1, false));
+  zones.push(helix(1*WINDOW_WIDTH*scale, 0, 2, 1, true));
 }
 
 function draw() {
@@ -46,6 +53,74 @@ function draw() {
   }
 
   if (showWindowFrame) drawWindowsOutline();
+}
+
+function helix(x, y, m, n, ortho) {
+  let cnv = createGraphics(m*WINDOW_WIDTH*scale, n*WINDOW_HEIGHT*scale, WEBGL);
+  cnv.noStroke();
+  cnv.colorMode(HSB);
+  cnv.translate(-cnv.width/2, -cnv.height/2);
+  if (ortho) cnv.ortho();
+  let wMargin = WINDOW_WIDTH*scale/10; // margin on the sides
+  let w = cnv.width-wMargin*2; // width of each function call rectangle
+  let h = 20; // height of each function call rectangle
+  let hGap = h/8; // gap between rectangles vertically
+  cnv.textSize(h*3/4);
+  cnv.textFont(myFont);
+  cnv.textAlign(CENTER, CENTER);
+
+  return {
+    x: x,
+    y: y,
+    m: m,
+    n: n,
+    cnv: cnv,
+    wMargin: wMargin,
+    w: w,
+    h: h,
+    hGap: hGap,
+    update: function(t) {
+      cnv.clear();
+
+      let x = this.wMargin, y = -t%this.h-height/3, i = floor(t/this.h);
+
+      while (y < cnv.height+cnv.height/3) {
+        let d = data.draw_trace[(i++)%trace_len];
+        let [sup, dep] = getSupAndDep(d.name);
+
+        cnv.push();
+
+        cnv.translate(x+this.w/2, y+this.h/2, 0);
+        cnv.rotateY(t*0*2 + y/50);
+        cnv.fill(get_sup_color(sup));
+        cnv.beginShape();
+        cnv.vertex(-this.w/2+this.wMargin, -this.h/2+this.hGap/2, 0);
+        cnv.vertex(-this.w/2+this.wMargin, this.h/2-this.hGap/2, 0);
+        cnv.fill(get_dep_color(dep));
+        cnv.vertex(this.w/2-this.wMargin, this.h/2-this.hGap/2, 0);
+        cnv.vertex(this.w/2-this.wMargin, -this.h/2+this.hGap/2, 0);
+        cnv.endShape();
+
+        let funcName = getActualName(d.name);
+        for (let wo of keywords) {
+          let idx = funcName.toLowerCase().indexOf(wo);
+          if (idx >= 0) {
+            cnv.fill(94);
+            //text(wo, w/2-wMargin/2, -hGap/2);
+            cnv.translate(0, -this.hGap, 1);
+            cnv.text(wo, 0, 0);
+            cnv.translate(0, 0, -2);
+            cnv.text(wo, 0, 0);
+            break;
+          }
+        }
+
+        cnv.pop();
+
+        y += this.h;
+      }
+    }
+  }
 }
 
 function determineScale() {
@@ -73,6 +148,14 @@ function drawWindowsOutline() {
   noStroke();
 }
 
+function get_dep_color(dep) {
+  return dep_colors.get(all_deps.indexOf(dep), 1);
+}
+
+function get_sup_color(sup) {
+  return sup_colors.get(all_sups.indexOf(sup), 1);
+}
+
 function getSupAndDep(s) {
   // s: a string corresponding to a method call, of the form "[eventual prefix]/[supplier].[dependency].[actual function name]"
   // return: an array, the first value is the supplier, the second is the dependency
@@ -86,12 +169,10 @@ function getSupAndDep(s) {
   return [supplier, dependency];
 }
 
-function initHelix() {
-
-}
-
-function runHelix() {
-
+function getActualName(s) {
+  // s: a string corresponding to a method call, of the form "[eventual prefix]/[supplier].[dependency].[actual function name]"
+  // return: the actual name of the function
+  return s.slice(s.lastIndexOf(".")+1, s.length);
 }
 
 function keyPressed() {
