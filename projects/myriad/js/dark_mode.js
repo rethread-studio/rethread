db536cbfb4fac2f638d18258c5e9a46b7f459410
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2023-10-12 16:43:50 +0200

    add glitch effects + misc changes

--- projects/myriad/js/dark_mode.js
+++ projects/myriad/js/dark_mode.js
@@ -1,3 +1,11 @@
+window.addEventListener('load', function() {
+    let mode = localStorage.getItem("mode");
+    if (mode == "dark") {
+        let element = document.body;
+        element.classList.toggle("dark-mode");
+    }
+});
+
 function toggle_dark_mode() {
     let element = document.body;
     element.classList.toggle("dark-mode");
@@ -5,11 +13,3 @@ function toggle_dark_mode() {
     if (mode == "dark") localStorage.setItem("mode", "light");
     else localStorage.setItem("mode", "dark");
 }
-
-window.onload = function() {
-    let mode = localStorage.getItem("mode");
-    if (mode == "dark") {
-        let element = document.body;
-        element.classList.toggle("dark-mode");
-    }
-};
