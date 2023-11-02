52334eb108e0b8e78208bb5adb523b50b5ca28a0
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-11-02 17:50:41 +0100

    More sonification ecperiments

--- code/NWL2023/trace_sonifier/bin/audify.rs
+++ code/NWL2023/trace_sonifier/bin/audify.rs
@@ -7,7 +7,7 @@ use knyst::{
     prelude::*,
     sphere::{KnystSphere, SphereSettings},
 };
-use trace_sonifier::binary_instructions::{self, machine_code};
+use trace_sonifier::binary_instructions::{self, machine_code, machine_code_instruction_only};
 
 fn main() -> Result<()> {
     // Start knyst
@@ -18,15 +18,16 @@ fn main() -> Result<()> {
         std::fs::read_to_string("/home/erik/Nextcloud/reinverse_traces/inverse_test_trace.txt")?;
 
     let machine_code = machine_code(&trace)?;
+    // let machine_code = machine_code_instruction_only(&trace)?;
     // Convert from 8bit unsigned to f32 samples
     let buffer: Vec<_> = machine_code
         .into_iter()
-        .map(|byte| (byte as u16 - (u8::MAX / 2) as u16) as f32 / (u8::MAX / 2) as f32)
+        .map(|byte| (byte as i16 - (u8::MAX / 2) as i16) as f32 / (u8::MAX / 2) as f32)
         .collect();
 
     // let buffer = Buffer::from_vec(buffer, 48000.);
     let mut buffer = Buffer::from_vec_interleaved(buffer, 1, 48000.);
-    // buffer.save_to_disk("/home/erik/buf_org.wav")?;
+    buffer.save_to_disk("/home/erik/buf_org.wav")?;
     buffer.remove_dc();
     // buffer.save_to_disk("/home/erik/buf_dc_removed.wav")?;
 
--- code/NWL2023/trace_sonifier/src/binary_instructions.rs
+++ code/NWL2023/trace_sonifier/src/binary_instructions.rs
@@ -22,3 +22,21 @@ pub fn machine_code(trace: &str) -> Result<Vec<u8>> {
 
     Ok(buf)
 }
+
+pub fn machine_code_instruction_only(trace: &str) -> Result<Vec<u8>> {
+    let mut buf = vec![];
+    let lines = trace.lines();
+    for line in lines.into_iter().filter(|l| &l[0..3] == "[I]") {
+        // Find the bytes
+        let mut byte_str = &line[74..];
+        if byte_str.chars().nth(0) == Some(']') {
+            byte_str = &byte_str[1..];
+        }
+        for b in byte_str.split_ascii_whitespace().take(1) {
+            let byte = hex::decode(b).wrap_err_with(|| format!("byte: {b}, line: {line}"))?;
+            buf.push(byte[0]);
+        }
+    }
+
+    Ok(buf)
+}
