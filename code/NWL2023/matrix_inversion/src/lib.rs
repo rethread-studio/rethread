5e1b3680f0d610bac811ace759e1a53d92c88840
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-11-06 10:54:05 +0100

    reinverse score

--- code/NWL2023/matrix_inversion/src/lib.rs
+++ code/NWL2023/matrix_inversion/src/lib.rs
@@ -5,17 +5,17 @@ pub use nalgebra::SquareMatrix;
 use nalgebra::{ComplexField, DMatrix, U32};
 use serde::{Deserialize, Serialize};
 include!(concat!(env!("OUT_DIR"), "/matrices.rs"));
-use matrices::RANDOM;
-use matrices::LOWER_TRIANGULAR;
 use matrices::DIAGONAL;
+use matrices::LOWER_TRIANGULAR;
+use matrices::RANDOM;
 use matrices::UPPER_TRIANGULAR;
-
 
 #[derive(Clone, Copy, Serialize, Debug)]
 enum Operation {
     Inverse,
     Normalize,
     Multiply,
+    SVD,
 }
 
 impl Operation {
@@ -23,19 +23,27 @@ impl Operation {
         &self,
         m0: DMatrix<T>,
         m1: Option<DMatrix<T>>,
-
-    ) -> Option<DMatrix<T>> {
+    ) -> Option<Vec<DMatrix<T>>> {
         match self {
-            Operation::Inverse => m0.try_inverse(),
-            Operation::Normalize => Some(m0.normalize()),
+            Operation::Inverse => {
+                if let Some(m) = m0.try_inverse() {
+                    Some(vec![m])
+                } else {
+                    None
+                }
+            }
+            Operation::Normalize => Some(vec![m0.normalize()]),
             Operation::Multiply => {
                 let m1 = m1.unwrap();
-                Some(m0.mul(m1))
-            },
+                Some(vec![m0.mul(m1)])
+            }
+            Operation::SVD => {
+                let svd = m0.svd(true, true);
+                Some(vec![svd.u.unwrap().into(), svd.v_t.unwrap().into()])
+            }
         }
     }
 }
-
 
 type FlatMatrix = Vec<f64>;
 #[derive(Serialize)]
@@ -52,8 +60,7 @@ struct ScoreObject {
 }
 
 pub fn test_main() -> Result<()> {
-
-    let operation = Operation::Inverse;
+    let operation = Operation::Multiply;
     // let matrix_values = (0..(32 * 32))
     //     .map(|_| rng.gen_range(0.0..1.0))
     //     .collect::<Vec<f64>>();
@@ -62,20 +69,35 @@ pub fn test_main() -> Result<()> {
     //     .flat_map(|row| row.iter())
     //     .cloned()
     //     .collect();
-    let matrix_values = DIAGONAL[0];
-    let source_matrix = DMatrix::<f64>::from_column_slice(32, 32, &matrix_values[..]);
-    let result = operation.apply(source_matrix, None);
+    let matrix_values0 = RANDOM[73];
+    let source_matrix0 = DMatrix::<f64>::from_column_slice(32, 32, &matrix_values0[..]);
+    let matrix_values1 = LOWER_TRIANGULAR[53];
+    let source_matrix1 = DMatrix::<f64>::from_column_slice(32, 32, &matrix_values1[..]);
+    let result = operation.apply(source_matrix0, Some(source_matrix1));
     if let Some(res) = result {
-        // let source_matrix_name = format!("diagonal_0");
-        // let so = ScoreObject {
-        //     operation,
-        //     source: vec![SourceMatrix{ matrix: matrix_values.to_vec(), name: source_matrix_name.clone() }],
-        //     result: vec![res.data.as_vec().clone()],
-        //     trace_name: format!("{:?}_{source_matrix_name}", operation),
-        // };
-        // let json = serde_json::to_string(&so)?;
-        // println!("{json}");
-        black_box(res); // avoid optimising the whole thing away
+        let source_matrix_name = format!("random_73");
+        let result = res
+            .into_iter()
+            .map(|mat| mat.data.as_vec().clone())
+            .collect();
+        let so = ScoreObject {
+            operation,
+            source: vec![
+                SourceMatrix {
+                    matrix: matrix_values0.to_vec(),
+                    name: source_matrix_name.clone(),
+                },
+                SourceMatrix {
+                    matrix: matrix_values1.to_vec(),
+                    name: "lower_triangular_53".to_string(),
+                },
+            ],
+            result,
+            trace_name: format!("{:?}_{source_matrix_name}", operation),
+        };
+        let json = serde_json::to_string(&so)?;
+        println!("{json}");
+        // black_box(res); // avoid optimising the whole thing away
     } else {
         // println!("Operation failed");
     }
