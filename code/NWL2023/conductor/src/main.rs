f8de7c5aee23e68bb21eecf61a448b31e535555a
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-11-15 17:01:59 +0100

    reinverse conductor

--- code/NWL2023/conductor/src/main.rs
+++ code/NWL2023/conductor/src/main.rs
@@ -4,11 +4,20 @@ use config::Config;
 use log::{error, info};
 use nannou_osc::{Connected, Sender, Type};
 mod config;
+use rand::rngs::StdRng;
 use rand::thread_rng;
 use rand::Rng;
+use rand::SeedableRng;
 use simple_logger::SimpleLogger;
 
-fn main() {
+enum Messages {
+    StartMovement(usize),
+    Pulse(i32),
+    Break,
+    BeamWidth(f32),
+}
+#[tokio::main]
+async fn main() {
     SimpleLogger::new().init().unwrap();
     let num_movements = 8;
     let mut rng = thread_rng();
@@ -23,19 +32,43 @@ fn main() {
         error!("Failed to load configuration");
         Config::empty()
     };
+    let (mtx, mut mrx) = tokio::sync::mpsc::unbounded_channel();
     let mut sender = OscSender::new(&settings);
-    sender.send_start_new_movement(0);
+    tokio::task::spawn_blocking(move || loop {
+        let mess_to_send = mrx.blocking_recv();
+        if let Some(m) = mess_to_send {
+            match m {
+                Messages::StartMovement(mvt) => sender.send_start_new_movement(mvt as i32),
+                Messages::Pulse(pulse) => sender.send_pulse(pulse),
+                Messages::Break => sender.send_break(),
+                Messages::BeamWidth(beam) => sender.send_beam_width(beam),
+            }
+        }
+    });
+
+    {
+        let mtx = mtx.clone();
+        tokio::task::spawn(async move {
+            let mut rng: StdRng = SeedableRng::from_entropy();
+            loop {
+                mtx.send(Messages::BeamWidth(rng.gen_range(0.1..1.0)));
+                tokio::time::sleep(Duration::from_millis(2000)).await;
+            }
+        });
+    }
 
     loop {
-        sender.send_pulse(pulse_counter);
+        mtx.send(Messages::Pulse(pulse_counter));
         if movement_start.elapsed() >= movement_time {
-            sender.send_break();
-            std::thread::sleep(break_time);
-            sender.send_start_new_movement(rng.gen_range(0..num_movements));
+            mtx.send(Messages::Break);
+            tokio::time::sleep(break_time).await;
+            mtx.send(Messages::StartMovement(
+                rng.gen_range(0..num_movements as usize),
+            ));
             movement_start = Instant::now();
             pulse_counter = 0;
         } else {
-            std::thread::sleep(movement_time / num_pulses_per_movement);
+            tokio::time::sleep(movement_time / num_pulses_per_movement).await;
             pulse_counter += 1;
         }
     }
@@ -79,6 +112,14 @@ impl OscSender {
         for sender in &mut self.senders {
             let addr = "/break";
             let args = vec![];
+            sender.send((addr, args)).ok();
+        }
+    }
+    pub fn send_beam_width(&mut self, value: f32) {
+        info!("Send beam: {value}");
+        for sender in &mut self.senders {
+            let addr = "/beam_width";
+            let args = vec![Type::Float(value)];
             sender.send((addr, args)).ok();
         }
     }
