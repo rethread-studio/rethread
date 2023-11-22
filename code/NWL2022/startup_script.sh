8d1ad6fdf4f4d3d9626a0188fab5e15257741d7f
Erik Natanael Gustafsson <erik@eriknatanael.com>
2023-11-22 11:46:38 +0100

    Small tweaks

--- code/NWL2022/startup_script.sh
+++ code/NWL2022/startup_script.sh
@@ -23,15 +23,15 @@ tmux split-window -t $session:$window -v
 # tmux send-keys -t $session:$window "export XAUTHORITY=/home/reth/.Xauthority" C-m
 # jackd -R -d alsa -d hw:US20x20 -n 3 -r 48000 -p 128
 #
-tmux send-keys -t $session:$window "jackd -R -d alsa -d hw:US20x20 -n 3 -r 48000 -p 128 &" C-m
-tmux send-keys -t $session:$window "export DISPLAY=:0" C-m
-tmux send-keys -t $session:$window "sleep 1" C-m
-tmux send-keys -t $session:$window "cd /home/reth/Documents/rethread/code/NWL2022/supercollider && sclang -D main.scd > /home/reth/sc_unfold_log.txt" C-m
-tmux split-window -t $session:$window -v
-tmux send-keys -t $session:$window "cd /home/reth/Documents/rethread/ && python3 -m http.server 8800" C-m
+# tmux send-keys -t $session:$window "jackd -R -d alsa -d hw:US20x20 -n 3 -r 48000 -p 128 &" C-m
+# tmux send-keys -t $session:$window "export DISPLAY=:0" C-m
+# tmux send-keys -t $session:$window "sleep 1" C-m
+# tmux send-keys -t $session:$window "cd /home/reth/Documents/rethread/code/NWL2022/supercollider && sclang -D main.scd > /home/reth/sc_unfold_log.txt" C-m
+# tmux split-window -t $session:$window -v
+# tmux send-keys -t $session:$window "cd /home/reth/Documents/rethread/ && python3 -m http.server 8800" C-m
 tmux split-window -t $session:$window -v
 sleep 3
-tmux send-keys -t $session:$window "cd /home/reth/Documents/rethread/code/NWL2022/rust/unfold_control && cargo run --release -- --trace /home/reth/Documents/unfold_traces/data-varna-startup-shutdown --osc --headless
+tmux send-keys -t $session:$window "cd /home/reth/Documents/rethread/code/NWL2022/rust/unfold_control && cargo run --release -- --trace /home/reth/Documents/unfold_traces/data-jedit-with-marker --osc --headless
 " C-m
 
 echo "Startup done\n"
