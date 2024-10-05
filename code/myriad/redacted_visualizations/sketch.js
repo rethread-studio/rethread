cd3a1a0160f5662e9e249be583458cff7328898f
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2024-10-05 12:42:57 +0200

    misc updates and changes

--- code/myriad/redacted_visualizations/sketch.js
+++ code/myriad/redacted_visualizations/sketch.js
@@ -3,10 +3,10 @@ p5.disableFriendlyErrors = true;
 let inconsolataRegular, inconsolataSemiBold;
 
 const COMPUTER = 0, KINDLE = 1, TABLET = 2, SURFACE = 3, LAPTOP = 4;
-let type = LAPTOP;
+let type = TABLET;
 
 let w = 1024;
-let ratio = type == COMPUTER ? 3/4 : (type == KINDLE ? Math.sqrt(2) : (type == TABLET ? 1.6 : (type == TABLET ? 9/16 : 0.625)));
+let ratio = type == COMPUTER ? 3 / 4 : (type == KINDLE ? Math.sqrt(2) : (type == TABLET ? 1.6 : (type == SURFACE ? 9 / 16 : 0.625)));
 
 let project;
 
@@ -26,29 +26,30 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(w, w*ratio);
+    createCanvas(w, w * ratio);
     pixelDensity(1);
     noLoop();
     noStroke();
 
-    let possibilities = ["macOS", "Windows", "Google Docs", "Adobe Photoshop", "Adobe Illustrator", "Safari", "Facebook", "ChatGPT", "YouTube", "Gmail"];
+    let possibilities = ["macOS", "Google Docs", "Adobe Photoshop", "Adobe Illustrator", "Safari", "Facebook", "YouTube", "Gmail"];
     if (type == KINDLE) possibilities = ["Amazon.se"];
-    if (type == TABLET) possibilities = ["Instagram", "WhatsApp", "iOS"];
+    if (type == TABLET) possibilities = ["Instagram", "WhatsApp", "iOS", "ChatGPT"];
+    if (type == SURFACE) possibilities = ["Windows", "Zoom"];
     project = random(possibilities);
     //project = possibilities[0];
 
-    textSize = max(width, height)/(type == KINDLE ? 22 : 45);
-    titleTextSize = textSize*1.5;
-    lineHeight = textSize*1.3;
-    sideMargin = titleTextSize*1.12;
-    nLines = round(height/lineHeight);
+    textSize = max(width, height) / (type == KINDLE ? 22 : 45);
+    titleTextSize = textSize * 1.5;
+    lineHeight = textSize * 1.3;
+    sideMargin = titleTextSize * 1.12;
+    nLines = 1+round(height / lineHeight);
 
     let marginToGap = 4;
     textFont(inconsolataRegular, textSize);
     columnWidth = textWidth("a".repeat(15));
     nColumns = (type == COMPUTER || type == SURFACE || type == LAPTOP) ? 5 : (type == KINDLE ? 1 : 3);
-    wGap = (width-2*sideMargin-nColumns*columnWidth)/(2*marginToGap+nColumns-1);
-    wMargin = marginToGap*wGap;
+    wGap = (width - 2 * sideMargin - nColumns * columnWidth) / (2 * marginToGap + nColumns - 1);
+    wMargin = marginToGap * wGap;
 }
 
 function draw() {
@@ -56,36 +57,36 @@ function draw() {
 
     fill(type == KINDLE ? "white" : "orange");
     rect(0, 0, sideMargin, height);
-    rect(width-sideMargin, 0, sideMargin, height);
+    rect(width - sideMargin, 0, sideMargin, height);
 
     fill("black");
     textFont(inconsolataSemiBold, titleTextSize);
     textAlign(CENTER, TOP);
 
     push();
-    translate(0, height/2);
-    rotate(-PI/2);
+    translate(0, height / 2);
+    rotate(-PI / 2);
     text(project, 0, 0);
     pop();
 
     push();
-    translate(width, height/2);
-    rotate(PI/2);
+    translate(width, height / 2);
+    rotate(PI / 2);
     text(project, 0, 0);
     pop();
 
     fill("white");
     textFont(inconsolataRegular, textSize);
     textAlign(CENTER, TOP);
-    
+
     let y = 0;
     for (let i = 0; i < nLines; i++) {
-        let x = sideMargin+wMargin+columnWidth/2;
+        let x = sideMargin + wMargin + columnWidth / 2;
         for (let j = 0; j < nColumns; j++) {
             let contributor = "▓".repeat(round(randomTriangle(1, 15)));
-            if (i == ~~(nLines/2-1) && j == ~~(nColumns/2)) contributor = "information";
-            if (i == ~~(nLines/2) && j == ~~(nColumns/2)) contributor = "not";
-            if (i == ~~(nLines/2+1) && j == ~~(nColumns/2)) contributor = "available";
+            if (i == ~~(nLines / 2 - 1) && j == ~~(nColumns / 2)) contributor = "information";
+            if (i == ~~(nLines / 2) && j == ~~(nColumns / 2)) contributor = "not";
+            if (i == ~~(nLines / 2 + 1) && j == ~~(nColumns / 2)) contributor = "available";
             text(contributor, x, y);
 
             x += wGap + columnWidth;
@@ -95,5 +96,5 @@ function draw() {
 }
 
 function randomTriangle(a, b) {
-    return a + (b - a) * (random()-random()+1) / 2;
+    return a + (b - a) * (random() - random() + 1) / 2;
 }
