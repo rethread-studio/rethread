ef04b8ae82ede62705e609431f8496e4ae8ac28e
bbaudry <baudry@kth.se>
2025-03-23 18:27:16 -0400

    subtitles that show the html code of the page

--- code/algorithmicart2025-vernissage/subtitles.js
+++ code/algorithmicart2025-vernissage/subtitles.js
@@ -1,28 +1,49 @@
-let page, index
+let page, index, nblines
+
+
+window.addEventListener("load", function() {
+    // on page load: fetch text in index.html and display two lines every 3 second
+    getData()
+    index=0
+    window.setInterval(showcode, 3000)
+});
 
 function getData() {
     fetch("index.html")
         .then((res) => res.text())
         .then((text) => {
+            //page is an array that includes all text found in "index.html"; each line in the file is a separate element in the array
             page = text.split("\n")
+            nblines = page.length
         })
         .catch((e) => console.error(e));
 }
-window.addEventListener("load", function() {
-    getData()
-    index=0
-    window.setInterval(showcode, 3000)
-
-});
-
-
-
 
 function showcode() {
-    document.getElementById("sub").innerHTML = "vera" + "<br>" + "molnar "+page[index].toString();
+    document.getElementById("sub1").innerHTML = escapeHTML(page[index]);
+    document.getElementById("sub2").innerHTML = escapeHTML(page[index+1]);
+    page[index].length!=0
     console.log(page[index])
-    index++
+    console.log(page[index+1])
+    if(index+1==nblines-1){
+        index=0
+    }
+    else{
+        index+=2
+    }
 }
 
 
 
+const escapeHTML = str =>
+    str.replace(
+      /[&<>'"]/g,
+      tag =>
+        ({
+          '&': '&amp;',
+          '<': '&lt;',
+          '>': '&gt;',
+          "'": '&#39;',
+          '"': '&quot;'
+        }[tag] || tag)
+    );
